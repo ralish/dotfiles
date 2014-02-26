4cfd701d7ec35e7bed4c24c4e049e46e6bb7ee74
Samuel D. Leslie <sdl@nexiom.net>
2014-02-27 02:33:39 +1100

    Improved common shell configuration esp. for OS X

--- sh/common.sh
+++ sh/common.sh
@@ -1,15 +1,23 @@
 # Some general cnfiguration common to all shells
 # Should be compatible with: sh, bash, ksh, zsh
 
-# Customise our path
-export PATH=~/bin:$PATH
+# Additional locations to prefix to our PATH
+EXTRA_PATHS="~/bin"
 
 # Our preferred text editors ordered by priority
 EDITOR_PRIORITY="vim vi nano pico"
 
+# OS X configuration
+if [ $(uname -s) = "Darwin" ]; then
+	. ~/dotfiles/sh/osx.sh
+fi
+
+# Customise our path
+export PATH="$EXTRA_PATHS:$PATH"
+
 # Figure out which editor to default to
-for editor in `echo $EDITOR_PRIORITY`; do
-	editor_path=`command -v $editor`
+for editor in $EDITOR_PRIORITY; do
+	editor_path=$(command -v $editor)
 	if [ -n "$editor_path" ]; then
 		export EDITOR="$editor_path"
 		export VISUAL="$editor_path"
