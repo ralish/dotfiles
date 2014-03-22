3fa3271fcebbc3ab6b5ce7226f291c96efc2598c
Samuel D. Leslie <sdl@nexiom.net>
2014-03-22 04:35:01 +0000

    Clear the console on exit if possible

--- sh/logout.sh
+++ sh/logout.sh
@@ -8,3 +8,8 @@ if [ -d $HOME/.ecryptfs ]; then
 	fi
 fi
 
+# Clear the console on exit if this is not a nested shell session
+if [ "$SHLVL" = 1 ]; then
+	[ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
+fi
+
