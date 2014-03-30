172991da34fac306885f152c228ea496206847a9
Samuel D. Leslie <sdl@nexiom.net>
2014-03-31 01:33:05 +1100

    Add Sublime Text alias if we're working in Cygwin

--- sh/systems/cygwin.sh
+++ sh/systems/cygwin.sh
@@ -3,4 +3,12 @@
 # Because I never remember the '-s' parameter
 alias ssh-agent-cyg='eval $(ssh-agent -s)'
 
+# Add alias for Sublime Text
+SublRegPath='/proc/registry/HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall/Sublime Text 2_is1/InstallLocation'
+SublBinName='sublime_text.exe'
+if [ -f "$SublRegPath" ]; then
+	SublDirPath=$(cat "$SublRegPath" | sed 's/^\([A-Z]\):/\/cygdrive\/\1/' | sed 's/\\/\//g')
+	alias subl="\"$SublDirPath$SublBinName\""
+fi
+
 # vim: syntax=sh ts=4 sw=4 sts=4 sr noet
