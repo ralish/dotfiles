bd6bd855db23f514a07434ab7628b2923161dea7
Samuel D. Leslie <sdl@nexiom.net>
2014-05-02 21:26:39 +1000

    Not sure why I felt the need to have all Env Vars in ALL CAPS

--- cmd/SetupEnv.cmd
+++ cmd/SetupEnv.cmd
@@ -1,27 +1,27 @@
 @ECHO OFF
 ECHO Making Command Prompt suck slightly less...
 
-REM Uncomment to enable verbose mode
-REM SET VERBOSE=YES
-
 REM So that we can safely run via AutoRun
-REM Use something like: IF NOT DEFINED SETUPENV Path\To\SetupEnv.Cmd
-SET SETUPENV=YES
+REM Use something like: IF NOT DEFINED SetupEnv Path\To\SetupEnv.Cmd
+SET SetupEnv=Yes
 
-REM Because I'm tired of forgetting this
+REM Uncomment to enable verbose mode
+REM SET SetupEnvVerbose=Yes
+
+REM Because I'm tired of forgetting Cmd is not a *nix shell
 DOSKEY clear=cls
 DOSKEY ls=dir $*
 DOSKEY man=help $*
 DOSKEY which=where $*
 
 REM Add alias for Sublime Text
-SET SUBLREGPATH=HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Sublime Text 2_is1
-SET SUBLBINNAME=sublime_text.exe
-REG QUERY "%SUBLREGPATH%" /v InstallLocation > NUL 2>&1
+SET SublRegPath=HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Sublime Text 2_is1
+SET SublBinName=sublime_text.exe
+REG QUERY "%SublRegPath%" /v InstallLocation > NUL 2>&1
 IF NOT ERRORLEVEL 1 (
-    FOR /F "tokens=2*" %%a IN ('REG QUERY "%SUBLREGPATH%" /v InstallLocation ^| FINDSTR /R "[a-z]:\\.*\\$"') DO @SET SUBLDIRPATH=%%b
+    FOR /F "tokens=2*" %%a IN ('REG QUERY "%SublRegPath%" /v InstallLocation ^| FINDSTR /R "[a-z]:\\.*\\$"') DO @SET SublDirPath=%%b
 ) ELSE (
-    IF DEFINED VERBOSE ECHO Couldn't locate Sublime Text install; not adding 'subl' alias.
+    IF DEFINED SetupEnvVerbose ECHO Couldn't locate Sublime Text install; not adding 'subl' alias.
 )
-IF DEFINED SUBLDIRPATH DOSKEY subl="%SUBLDIRPATH%%SUBLBINNAME%" $*
-FOR /F "delims==" %%i IN ('SET SUBL') DO @SET %%i=
+IF DEFINED SublDirPath DOSKEY subl="%SublDirPath%%SublBinName%" $*
+FOR /F "delims==" %%i IN ('SET Subl') DO @SET %%i=
