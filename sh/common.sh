912fb599c83d3224cadad9672e053e630f29668f
Samuel D. Leslie <sdl@nexiom.net>
2014-06-15 06:57:28 +0000

    Better handling of constructing our PATH

--- sh/common.sh
+++ sh/common.sh
@@ -2,25 +2,57 @@
 # Should be compatible with at least: sh, bash, ksh, zsh
 
 # Additional locations to prefix to our PATH
-EXTRA_PATHS="~/bin"
+EXTRA_PATHS=''
 
 # Our preferred text editors ordered by priority
-EDITOR_PRIORITY="vim vi nano pico"
+EDITOR_PRIORITY='vim vi nano pico'
 
-# Update our path immediately as some subsequent scripts may depend on it
-export PATH="$EXTRA_PATHS:$PATH"
+# Preserve our original PATH for use in some later munging
+original_path="$PATH"
+
+# Quick and dirty function to help manage adding to the PATH
+function path_add_prefix() {
+    if [ $# -eq 2 ]; then
+        if [ -n "$1" -a -n "$2" ]; then
+            echo "$1:$2"
+        else
+            echo "$1$2"
+        fi
+    else
+        echo 'Called path_add_prefix with an invalid number of parameters!'
+    fi
+}
+
+# Add any extra paths before we run the system specific stuff
+path_changes_general=$(path_add_prefix "$EXTRA_PATHS" "$path_changes_general")
+
+# If a bin directory exists in our home directory then add it
+if [ -d "$HOME/bin" ]; then
+    path_changes_general=$(path_add_prefix "$HOME/bin" "$path_changes_general")
+fi
+
+# Update the PATH with any changes we've recorded
+if [ -n "$path_changes_general" ]; then
+    export PATH="$path_changes_general:$PATH"
+fi
 
 # Operating system and environment specific configurations
 if [[ $(uname -s) == CYGWIN_NT-* ]]; then
     source "$HOME/dotfiles/sh/systems/cygwin.sh"
-elif [ $(uname -s) = "Darwin" ]; then
+elif [ $(uname -s) = 'Darwin' ]; then
     source "$HOME/dotfiles/sh/systems/osx.sh"
 fi
 
-# Pedantic fix to ensure our earlier extra paths are first in the path
-extra_paths_escaped=$(echo $EXTRA_PATHS | sed 's/\//\\\//g')
-PATH=$(echo $PATH | sed "s/$extra_paths_escaped:*//g")
-export PATH="$EXTRA_PATHS:$PATH"
+# Construct the final PATH with both the general and system changes
+if [ -n "$path_changes_system" ]; then
+    path_changes="$path_changes_system"
+fi
+if [ -n "$path_changes_general" ]; then
+    path_changes=$(path_add_prefix "$path_changes_general" "$path_changes")
+fi
+if [ -n "$path_changes" ]; then
+    export PATH="$path_changes:$original_path"
+fi
 
 # Figure out which editor to default to
 for editor in $(echo $EDITOR_PRIORITY); do
--- sh/systems/osx.sh
+++ sh/systems/osx.sh
@@ -1,10 +1,10 @@
 # OS X shell configuration
 
 # Prefer GNU coreutils?
-USE_GNU_COREUTILS="true"
+USE_GNU_COREUTILS='true'
 
 # Some extra paths to include
-export PATH="/usr/local/bin:$PATH"
+EXTRA_SYS_PATHS='/usr/local/bin'
 
 # Tell ls to be colourful
 export CLICOLOR='1'
@@ -14,7 +14,13 @@ export LSCOLORS='Exfxcxdxbxegedabagacad'
 export GREP_OPTIONS='--color=auto'
 
 # Assume x86_64 arch for all compiles
-export ARCHFLAGS="-arch x86_64"
+export ARCHFLAGS='-arch x86_64'
+
+# If we defined any extra paths add them
+if [ -n "$EXTRA_SYS_PATHS" ]; then
+    path_changes_system="$EXTRA_SYS_PATHS"
+    export PATH="$path_changes_system:$PATH"
+fi
 
 # Load bash completion if it's present
 if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
@@ -27,29 +33,22 @@ if [ -f "$(brew --prefix)/etc/grc.bashrc" ]; then
 fi
 
 # Preference GNU coreutils over the system defaults
-coreutils_binpath="/usr/local/opt/coreutils/libexec/gnubin"
-coreutils_manpath="/usr/local/opt/coreutils/libexec/gnuman"
+coreutils_binpath='/usr/local/opt/coreutils/libexec/gnubin'
+coreutils_manpath='/usr/local/opt/coreutils/libexec/gnuman'
 if [ -n "$USE_GNU_COREUTILS" ]; then
     if [ -d "$coreutils_binpath" ]; then
-        export PATH="$coreutils_binpath:$PATH"
+        path_changes_system=$(path_add_prefix "$coreutils_binpath" "$path_changes_system")
+        export PATH="$path_changes_system:$PATH"
         if [ -d "$coreutils_manpath" ]; then
-            if [ -n "$MANPATH" ]; then
-                export MANPATH="$coreutils_manpath:$MANPATH"
-            else
-                export MANPATH="$coreutils_manpath"
-            fi
+            MANPATH=$(path_add_prefix "$coreutils_manpath" "$MANPATH")
         fi
     fi
 fi
 
 # Add Python site-packages to path if it's present
-python_sitepkgs="/usr/local/lib/python2.7/site-packages"
+python_sitepkgs='/usr/local/lib/python2.7/site-packages'
 if [ -d "$python_sitepkgs" ]; then
-    if [ -n "$PYTHONPATH" ]; then
-        export PYTHONPATH="$python_sitepkgs"
-    else
-        export PYTHONPATH="$python_sitepkgs:$PYTHONPATH"
-    fi
+    PYTHONPATH=$(path_add_prefix "$python_sitepkgs" $PYTHONPATH")
 fi
 
 # vim: syntax=sh ts=4 sw=4 sts=4 et sr
