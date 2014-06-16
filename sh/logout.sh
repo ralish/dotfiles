584c69e8167620864c07b98d717f0502f01208fa
Samuel D. Leslie <sdl@nexiom.net>
2014-06-16 04:58:46 +0000

    Hack to fix eCryptfs being incredibly stupid
    I'm really beginning to think the "Unix philosophy" is a poor fit for
    secure systems.

--- sh/logout.sh
+++ sh/logout.sh
@@ -1,8 +1,13 @@
-# If our homedir is encrypted with eCryptfs then don't unmount it on
-# exit if any tmux sessions are running
+# If our homedir is encrypted with eCryptfs then don't unmount it on exit if
+# any tmux sessions are running. This is more difficult than it should be as
+# eCryptfs doesn't decrement the active sessions counter on the shared memory
+# device if the auto-umount file isn't present. Yes, this is entirely stupid.
 if [ -d "$HOME/.ecryptfs" ]; then
     if $(tmux ls > /dev/null 2>&1); then
         rm -f "$HOME/.ecryptfs/auto-umount"
+        count=$(cat /dev/shm/ecryptfs-$USER-Private)
+        let count--
+        echo $count > /dev/shm/ecryptfs-$USER-Private
     else
         touch "$HOME/.ecryptfs/auto-umount"
     fi
