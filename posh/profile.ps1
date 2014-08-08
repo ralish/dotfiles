ef2aba5dd1bd6a24fc51e7d856bf66b8899af093
Samuel D. Leslie <sdl@nexiom.net>
2014-08-09 01:13:33 +1000

    Support handling of custom PoSh functions & scripts dirs

--- posh/profile.ps1
+++ posh/profile.ps1
@@ -1,3 +1,6 @@
+# Determine the parent directory of our profile script for use later
+$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
 # Load PSReadLine if we're running PoSh >= 3.0
 if ($PSVersionTable.PSVersion.Major -ge 3) {
     if ((Get-Module PSReadLine -ListAvailable) -and ($Host.Name -eq 'ConsoleHost')) {
@@ -34,8 +37,15 @@ if (Get-Command ssh-add.exe) {
     Write-Verbose "Couldn't locate ssh-add.exe binary; not adding SSH keys to ssh-agent."
 }
 
+# Source any function files in our Functions directory
+Get-ChildItem "$ScriptPath\Functions" | % { . $_.FullName }
+
+# Amend our Path to include our Scripts directory
+$env:Path += ";$ScriptPath\Scripts"
+
 # Source our custom aliases & functions
-$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $ScriptPath 'Aliases.ps1')
 . (Join-Path $ScriptPath 'Functions.ps1')
+
+# Clean-up
 Remove-Variable ScriptPath
