f333d36e1de021eba531978078e33d0a481fcda4
Samuel D. Leslie <sdl@nexiom.net>
2014-11-29 16:40:48 +1100

    Better yet, just turn our functions into independent scripts

--- sh/common.sh
+++ sh/common.sh
@@ -72,11 +72,9 @@ if [ -f "$HOME/dotfiles/sh/aliases.sh" ]; then
     source "$HOME/dotfiles/sh/aliases.sh"
 fi
 
-# If we defined a custom functions folder then include it
-if [ -d "$HOME/dotfiles/sh/functions" ]; then
-    for file in $(ls "$HOME/dotfiles/sh/functions"); do
-        source "$file"
-    done
+# If we defined a custom functions file then include it
+if [ -f "$HOME/dotfiles/sh/functions.sh" ]; then
+    source "$HOME/dotfiles/sh/functions.sh"
 fi
 
 # Disable toggling flow control (use ixany to re-enable)
