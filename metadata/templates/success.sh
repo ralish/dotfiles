486062aa9219aa9a27d3471a8220adf3b141e649
Samuel D. Leslie <sdl@nexiom.net>
2015-01-01 17:45:49 +1100

    Add detection templates for symlink usage

---
+++ metadata/templates/success.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Source in common metadata functions
+script_dir="$(dirname "${BASH_SOURCE[0]}")"
+source "$script_dir/../common.sh"
+
+exit $DETECTION_SUCCESS
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
