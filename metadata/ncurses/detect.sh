5db9f42f6106095a88f44fbab499fa959e814a73
Samuel D. Leslie <sdl@nexiom.net>
2015-01-01 15:04:18 +1100

    Much better reporting in dot-manage & notify of detection conditions

--- metadata/ncurses/detect.sh
+++ metadata/ncurses/detect.sh
@@ -8,6 +8,8 @@ elif [ "${kernel_name#*CYGWIN_NT}" != "$kernel_name" ]; then
     if ! cygcheck -c -d | egrep '^libncurses' > /dev/null; then
         exit 1
     fi
+else
+    exit 2
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
