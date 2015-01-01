60dc01734fdccc0302be9b037f4a2a00d260ff2a
Samuel D. Leslie <sdl@nexiom.net>
2015-01-01 18:36:53 +1100

    Add detection logic for Cygwin component

---
+++ metadata/cygwin/detect.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Source in common metadata functions
+script_dir="$(dirname "${BASH_SOURCE[0]}")"
+source "$script_dir/../common.sh"
+
+kernel_name=$(uname -s)
+if [ "${kernel_name#*CYGWIN_NT}" != "$kernel_name" ]; then
+    exit $DETECTION_SUCCESS
+fi
+
+exit $DETECTION_IGNORE
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
