ff0f5174f59d66997fda6f3b46ba3f19e5f3ffed
Samuel D. Leslie <sdl@nexiom.net>
2015-01-01 17:54:35 +1100

    Convert applicable metadata configs to use template symlinks

--- metadata/sh/detect.sh
+++ metadata/sh/detect.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-
-# Source in common metadata functions
-script_dir="$(dirname "${BASH_SOURCE[0]}")"
-source "$script_dir/../common.sh"
-
-exit $DETECTION_NEVER_INSTALL
-
-# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
+../templates/never_install.sh
