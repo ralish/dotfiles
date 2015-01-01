2cd4e91458400f23597b0e8a9dac7a1f0881c9be
Samuel D. Leslie <sdl@nexiom.net>
2015-01-01 13:51:11 +1100

    Handle Cygwin detection for ncurses & readline libraries

--- metadata/ncurses/detect.sh
+++ metadata/ncurses/detect.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-if ! dpkg --get-selections | egrep '^libncurses' > /dev/null; then
-    exit 1
+if [ "${kernel_name#*Linux}" != "$kernel_name" ]; then
+    if ! dpkg --get-selections | egrep '^libncurses' > /dev/null; then
+        exit 1
+    fi
+elif [ "${kernel_name#*CYGWIN_NT}" != "$kernel_name" ]; then
+    if ! cygcheck -c -d | egrep '^libncurses' > /dev/null; then
+        exit 1
+    fi
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
