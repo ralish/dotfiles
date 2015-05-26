2b386a2351269d8016c8352527374e51e53b523b
Samuel D. Leslie <sdl@nexiom.net>
2015-05-26 13:54:54 +1000

    Build an OpenSSH configuration suitable for the running version
    New OpenSSH releases frequently introduce configuration parameters or
    options which older versions won't simply ignore. As such, if we want to
    make use of these new parameters/options, we need to ensure they're not
    present in the configuration when using an older client which won't
    understand them.
    
    To handle this, we have a template configuration file for each supported
    OpenSSH version containing all of the valid configuration parameters,
    which is appended to our custom configuration which we share between all
    versions (at least for now). The result is stored as the actual config
    which OpenSSH will use. Not ideal, but still fairly elegant & flexible.

--- metadata/openssh/detect.sh
+++ metadata/openssh/detect.sh
@@ -8,6 +8,25 @@ if ! command -v ssh > /dev/null; then
     exit $DETECTION_NOT_AVAILABLE
 fi
 
+# Paths we need to know to build our configuration
+OPENSSH_DIR="$script_dir/../../openssh/.ssh"
+OPENSSH_CFG="$OPENSSH_DIR/config"
+TEMPLATE_DIR="$OPENSSH_DIR/templates"
+CUSTOM_CFG="$TEMPLATE_DIR/ssh_config"
+
+# Check for a supported version and build our config
+ssh_version=$(ssh -V 2>&1)
+if [[ $ssh_version =~ 'OpenSSH_5.9' ]]; then
+    head -n -1 "$CUSTOM_CFG" > "$OPENSSH_CFG"
+    tail -n +4 "$TEMPLATE_DIR/ssh_config.59" >> "$OPENSSH_CFG"
+elif [[ $ssh_version =~ 'OpenSSH_6.6' ]]; then
+    head -n -1 "$CUSTOM_CFG" > "$OPENSSH_CFG"
+    tail -n +4 "$TEMPLATE_DIR/ssh_config.59" >> "$OPENSSH_CFG"
+else
+    echo "Unsupported OpenSSH version detected: $ssh_version"
+    exit $DETECTION_NO_LOGIC
+fi
+
 exit $DETECTION_SUCCESS
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
