e4dfea943ce833ddb2fe1768f8fa091f5c4e97ad
Samuel D. Leslie <sdl@nexiom.net>
2015-07-26 16:26:04 +1000

    Super useful alias to determine manually installed packages in APT

---
+++ sh/apps/apt.sh
@@ -0,0 +1,11 @@
+# apt configuration
+
+if command -v apt > /dev/null; then
+    # Super useful alias to determine manually installed packages
+    # Via: https://askubuntu.com/questions/2389/generating-list-of-manually-installed-packages-and-querying-individual-packages
+    alias apt-pkgs='comm -23 <(apt-mark showmanual | sort -u) \
+                             <(gzip -dc /var/log/installer/initial-status.gz \
+                               | sed -n "s/^Package: //p" | sort -u)'
+fi
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
