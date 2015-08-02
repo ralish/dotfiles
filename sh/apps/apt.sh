81396b72a14d4c21b38a796f2af144cd14ccd1f5
Samuel D. Leslie <sdl@nexiom.net>
2015-08-03 01:19:58 +1000

    Note to self: apt is not a command (it's a package)

--- sh/apps/apt.sh
+++ sh/apps/apt.sh
@@ -1,6 +1,6 @@
 # apt configuration
 
-if command -v apt > /dev/null; then
+if command -v apt-get > /dev/null; then
     # Super useful alias to determine manually installed packages
     # Via: https://askubuntu.com/questions/2389/generating-list-of-manually-installed-packages-and-querying-individual-packages
     alias apt-pkgs='comm -23 <(apt-mark showmanual | sort -u) \
