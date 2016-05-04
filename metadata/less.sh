c82adf9a00260839a472f8ef0a0b35837c14074b
Samuel D. Leslie <sdl@nexiom.net>
2016-05-04 20:03:27 +1000

    Use a .lesskey file instead of env vars due to increased portability

---
+++ metadata/less.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Source in common metadata functions
+script_dir="$(dirname "${BASH_SOURCE[0]}")"
+source "$script_dir/templates/common.sh"
+
+if ! command -v lesskey > /dev/null; then
+    exit $DETECTION_NOT_AVAILABLE
+fi
+
+# Path to the lesskey configuration file
+LESSKEY="$script_dir/../less/.lesskey"
+
+# Generate the binary configuration file
+lesskey -- <(cat "$LESSKEY")
+
+exit $DETECTION_SUCCESS
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
--- sh/apps/less.sh
+++
@@ -1,8 +0,0 @@
-# less configuration
-
-if command -v less > /dev/null; then
-    # Options to pass to less
-    export LESS='-i -R'
-fi
-
-# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
