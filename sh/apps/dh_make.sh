20c5f1cc7f60e24335bff749763841a642733bcc
Samuel D. Leslie <sdl@nexiom.net>
2016-06-02 15:59:02 +1000

    Add initial dh_make configuration (very minimalist)

---
+++ sh/apps/dh_make.sh
@@ -0,0 +1,9 @@
+# dh_make configuration
+
+if command -v dh_make > /dev/null; then
+    # Full name & email for Debian package tools
+    export DEBFULLNAME='Samuel D. Leslie'
+    export DEBEMAIL='sdl@nexiom.net'
+fi
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
