5ee63392b495226e9ae6f1223babdae9299fd077
Samuel D. Leslie <sdl@nexiom.net>
2016-06-08 13:25:05 +1000

    Add our initial editline (libedit) configuration

---
+++ metadata/editline.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Source in common metadata functions
+script_dir="$(dirname "${BASH_SOURCE[0]}")"
+source "$script_dir/templates/common.sh"
+
+kernel_name=$(uname -s)
+if [ "${kernel_name#*Linux}" != "$kernel_name" ]; then
+    if ! dpkg --get-selections | egrep '^libedit' > /dev/null; then
+        exit $DETECTION_NOT_AVAILABLE
+    fi
+elif [ "${kernel_name#*CYGWIN_NT}" != "$kernel_name" ]; then
+    if ! cygcheck -c -d | egrep '^libedit' > /dev/null; then
+        exit $DETECTION_NOT_AVAILABLE
+    fi
+else
+    exit $DETECTION_NO_LOGIC
+fi
+
+exit $DETECTION_SUCCESS
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
