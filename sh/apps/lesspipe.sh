2b304eba05653ecc0fb87c2425929054a7ffefea
Samuel D. Leslie <sdl@nexiom.net>
2016-07-04 00:00:15 +1000

    Add shell integration for lesspipe to enable non-text input to less

---
+++ sh/apps/lesspipe.sh
@@ -0,0 +1,8 @@
+# lesspipe configuration
+
+if command -v lesspipe > /dev/null; then
+    # Make use of lesspipe for handling non-text input
+    eval "$(SHELL=/bin/sh lesspipe)"
+fi
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
