7d1a7f26e572a72ea1c6eb8c758e41627750a0e5
Samuel D. Leslie <sdl@nexiom.net>
2016-07-03 23:59:20 +1000

    Add shell integration for GCC to enable colour output

---
+++ sh/apps/gcc.sh
@@ -0,0 +1,8 @@
+# GCC configuration
+
+if command -v gcc > /dev/null; then
+    # Enable colour output
+    export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+fi
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
