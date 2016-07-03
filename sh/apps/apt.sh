3da02e343a23ba8148635e740e8e39749a9bc4f4
Samuel D. Leslie <sdl@nexiom.net>
2016-07-03 23:21:42 +1000

    Fix broken apt-pkgs alias due to incorrect quoting

--- sh/apps/apt.sh
+++ sh/apps/apt.sh
@@ -1,11 +1,11 @@
-# apt configuration
+# APT configuration
 
 if command -v apt-get > /dev/null; then
     # Super useful alias to determine manually installed packages
     # Via: https://askubuntu.com/questions/2389/generating-list-of-manually-installed-packages-and-querying-individual-packages
-    alias apt-pkgs='comm -23 <(apt-mark showmanual | sort -u) \
+    alias apt-pkgs="comm -23 <(apt-mark showmanual | sort -u) \
                              <(gzip -dc /var/log/installer/initial-status.gz \
-                               | sed -n "s/^Package: //p" | sort -u)'
+                               | sed -n \"s/^Package: //p\" | sort -u)"
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
