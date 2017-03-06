416d40836967e6a9f716a484eaae41cda1471da0
Samuel D. Leslie <sdl@nexiom.net>
2017-03-07 06:16:47 +1100

    Bunch of fixes to our PowerShell functions for best practice

--- posh/Functions.ps1
+++ posh/Functions.ps1
@@ -2,10 +2,12 @@ Function Connect-AllOffice365Services {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
-            [System.Management.Automation.PSCredential]$Credential,
+        [System.Management.Automation.Credential()][pscredential]$Credential,
+
         [Parameter(Mandatory=$true)]
-            [String]$SharePointDomainName,
-            [String]$SccCmdletsPrefix='Scc'
+        [String]$SharePointDomainName,
+
+        [String]$SccCmdletsPrefix='Scc'
     )
 
     Connect-Office365 -Credential $Credential
@@ -19,27 +21,27 @@ Function Connect-ExchangeOnline {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
-            [System.Management.Automation.PSCredential]$Credential
+        [System.Management.Automation.Credential()][pscredential]$Credential
     )
 
-    Write-Verbose 'Connecting to Exchange Online ...'
+    Write-Verbose -Message 'Connecting to Exchange Online ...'
     $ExchangeOnline = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri 'https://outlook.office365.com/powershell-liveid/' -Credential $Credential -Authentication 'Basic' -AllowRedirection
-    Import-PSSession $ExchangeOnline -DisableNameChecking
+    Import-PSSession -Session $ExchangeOnline -DisableNameChecking
 }
 
 Function Connect-Office365 {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
-            [System.Management.Automation.PSCredential]$Credential
+        [System.Management.Automation.Credential()][pscredential]$Credential
     )
 
     if (!(Get-Module -Name MSOnline -ListAvailable)) {
         throw 'Required module not available: MSOnline'
     }
 
-    Write-Verbose 'Connecting to Office 365 ...'
-    Import-Module MSOnline
+    Write-Verbose -Message 'Connecting to Office 365 ...'
+    Import-Module -Name MSOnline
     Connect-MsolService -Credential $Credential
 }
 
@@ -47,31 +49,33 @@ Function Connect-SecurityAndComplianceCenter {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
-            [System.Management.Automation.PSCredential]$Credential,
-            [String]$CmdletsPrefix='Scc'
+        [System.Management.Automation.Credential()][pscredential]$Credential,
+
+        [String]$CmdletsPrefix='Scc'
     )
 
-    Write-Verbose 'Connecting to Security and Compliance Center ...'
+    Write-Verbose -Message 'Connecting to Security and Compliance Center ...'
     $SCC = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri 'https://ps.compliance.protection.outlook.com/powershell-liveid/' -Credential $Credential -Authentication 'Basic' -AllowRedirection
-    Import-PSSession $SCC -Prefix $CmdletsPrefix
+    Import-PSSession -Session $SCC -Prefix $CmdletsPrefix
 }
 
 Function Connect-SharePointOnline {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
-            [System.Management.Automation.PSCredential]$Credential,
+        [System.Management.Automation.Credential()][pscredential]$Credential,
+
         [Parameter(Mandatory=$true)]
-            [String]$DomainName
+        [String]$DomainName
     )
 
     if (!(Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable)) {
         throw 'Required module not available: Microsoft.Online.SharePoint.PowerShell'
     }
 
-    Write-Verbose 'Connecting to SharePoint Online ...'
-    Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
-    $SPOUrl = "https://$DomainName-admin.sharepoint.com"
+    Write-Verbose -Message 'Connecting to SharePoint Online ...'
+    Import-Module -Name Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
+    $SPOUrl = ('https://{0}-admin.sharepoint.com' -f $DomainName)
     Connect-SPOService -Url $SPOUrl -Credential $Credential
 }
 
@@ -79,37 +83,37 @@ Function Connect-SkypeForBusinessOnline {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
-            [System.Management.Automation.PSCredential]$Credential
+        [System.Management.Automation.Credential()][pscredential]$Credential
     )
 
     if (!(Get-Module -Name SkypeOnlineConnector -ListAvailable)) {
         throw 'Required module not available: SkypeOnlineConnector'
     }
 
-    Write-Verbose 'Connecting to Skype for Business Online ...'
-    Import-Module SkypeOnlineConnector
+    Write-Verbose -Message 'Connecting to Skype for Business Online ...'
+    Import-Module -Name SkypeOnlineConnector
     $SkypeForBusinessOnline = New-CsOnlineSession -Credential $Credential
-    Import-PSSession $SkypeForBusinessOnline
+    Import-PSSession -Session $SkypeForBusinessOnline
 }
 
 # Convert a string to the Base64 form suitable for usage with PowerShell's "-EncodedCommand" parameter
 Function ConvertTo-PoShBase64 {
     Param(
         [Parameter(Position=1,Mandatory=$true)]
-            [String]$String
+        [String]$String
     )
 
-    [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("$String"))
+    [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($String))
 }
 
 # Convert a string from the Base64 form suitable for usage with PowerShell's "-EncodedCommand" parameter
 Function ConvertFrom-PoShBase64 {
     Param(
         [Parameter(Position=1,Mandatory=$true)]
-            [String]$String
+        [String]$String
     )
 
-    [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String("$String"))
+    [Text.Encoding]::Unicode.GetString([Convert]::FromBase64String($String))
 }
 
 # Neatly print out all users with UIDs (uses RFC2307 schema extensions)
@@ -131,21 +135,21 @@ Function Get-ADGroupGID {
 Function Get-EventLogTail {
     Param(
         [Parameter(Mandatory=$true)]
-            [String]$EventLog
+        [String]$EventLog
     )
 
     $idx1 = (Get-EventLog -LogName $EventLog -Newest 1).Index
     do {
         Start-Sleep -Seconds 1
         $idx2 = (Get-EventLog -LogName $EventLog -Newest 1).Index
-        Get-EventLog -LogName $EventLog -Newest ($idx2 - $idx1) | Sort Index
+        Get-EventLog -LogName $EventLog -Newest ($idx2 - $idx1) | Sort-Object -Property Index
         $idx1 = $idx2
     } while ($true)
 }
 
 # Find Office 365 users which have one or more disabled licenced services
 Function Get-Office365UsersWithDisabledServices {
-    $Users = Get-MsolUser | ? { $_.IsLicensed -eq $true}
+    $Users = Get-MsolUser | Where-Object { $_.IsLicensed -eq $true}
     foreach ($User in $Users) {
         $DisabledServices = @()
         $LicencedServices = $User.Licenses.ServiceStatus
@@ -157,9 +161,9 @@ Function Get-Office365UsersWithDisabledServices {
         }
 
         if ($DisabledServices.Count -gt 0) {
-            Write-Output ("{0} has the following disabled services:" -f $User.DisplayName)
+            Write-Output -InputObject ('{0} has the following disabled services:' -f $User.DisplayName)
             $DisabledServices
-            Write-Output ""
+            Write-Output -InputObject ''
         }
     }
 }
@@ -167,5 +171,5 @@ Function Get-Office365UsersWithDisabledServices {
 # The MKLINK command is actually part of the Command Processor (cmd.exe)
 # So we have a quick and dirty function below to invoke it via PowerShell
 Function mklink {
-    cmd /c mklink $args
+    & "$env:ComSpec" /c mklink $args
 }
