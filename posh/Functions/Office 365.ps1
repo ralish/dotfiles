c893a893e7200a4c1e8132e4fe4777ff41f9dd7c
Samuel D. Leslie <sdl@nexiom.net>
2017-10-11 11:44:04 +1100

    Lots of updates to Office 365 PowerShell helper functions
    Also adds MFA support for the Security & Compliance Center.

--- posh/Functions/Office 365.ps1
+++ posh/Functions/Office 365.ps1
@@ -1,41 +1,57 @@
 Function Connect-Office365Services {
+    [CmdletBinding(DefaultParameterSetName='MFA')]
     Param(
-        [Parameter(Mandatory)]
+        [Parameter(ParameterSetName='MFA')]
+        [ValidateNotNullOrEmpty()]
+        [String]$MfaUsername,
+
+        [Parameter(ParameterSetName='Standard',Mandatory)]
         [ValidateNotNull()]
         [System.Management.Automation.Credential()]
         [PSCredential]$Credential,
 
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [String]$SharePointTenantName,
-
-        [ValidateNotNullOrEmpty()]
-        [String]$SccCmdletsPrefix='Scc'
+        [String]$SharePointTenantName
     )
 
-    Write-Host -ForegroundColor Green -Object 'Connecting to Exchange Online ...'
-    Connect-ExchangeOnline -Credential $Credential
+    if ($PSCmdlet.ParameterSetName -eq 'MFA') {
+        Connect-ExchangeOnline -MfaUsername $MfaUsername
+    } else {
+        Connect-ExchangeOnline -Credential $Credential
+    }
 
-    Write-Host -ForegroundColor Green -Object 'Connecting to SharePoint Online ...'
-    Connect-SharePointOnline -TenantName $SharePointTenantName -Credential $Credential
+    if ($PSCmdlet.ParameterSetName -eq 'MFA') {
+        Connect-SecurityAndComplianceCenter -MfaUsername $MfaUsername
+    } else {
+        Connect-SecurityAndComplianceCenter -Credential $Credential
+    }
 
-    Write-Host -ForegroundColor Green -Object 'Connecting to Skype for Business Online ...'
-    Connect-SkypeForBusinessOnline -Credential $Credential
+    if ($PSCmdlet.ParameterSetName -eq 'MFA') {
+        Connect-SharePointOnline -TenantName $SharePointTenantName
+    } else {
+        Connect-SharePointOnline -TenantName $SharePointTenantName -Credential $Credential
+    }
 
-    Write-Host -ForegroundColor Green -Object 'Connecting to Security & Compliance Center ...'
-    Connect-Office365SecurityAndComplianceCenter -Credential $Credential -CmdletsPrefix $SccCmdletsPrefix
+    if ($PSCmdlet.ParameterSetName -eq 'MFA') {
+        Connect-SkypeForBusinessOnline -MfaUsername $MfaUsername
+    } else {
+        Connect-SkypeForBusinessOnline -Credential $Credential
+    }
 
-    Write-Host -ForegroundColor Green -Object 'Connecting to Centralized Deployment ...'
-    Connect-Office365CentralizedDeployment -Credential $Credential
+    if ($PSCmdlet.ParameterSetName -eq 'MFA') {
+        Write-Warning -Message "Unable to connect to Office 365 Centralized Deployment as it doesn't yet support MFA."
+    } else {
+        Connect-Office365CentralizedDeployment -Credential $Credential
+    }
 }
-
 
 Function Connect-ExchangeOnline {
     [CmdletBinding(DefaultParameterSetName='MFA')]
     Param(
         [Parameter(ParameterSetName='MFA')]
         [ValidateNotNullOrEmpty()]
-        [String]$UserPrincipalName,
+        [String]$MfaUsername,
 
         [Parameter(ParameterSetName='Standard',Mandatory)]
         [ValidateNotNull()]
@@ -44,36 +60,46 @@ Function Connect-ExchangeOnline {
     )
 
     if ($PSCmdlet.ParameterSetName -eq 'MFA') {
-        if (!(Get-Command -Name Connect-EXOPSSession -ErrorAction Ignore)) {
-            $ClickOnceAppsPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath 'Apps\2.0'
-            $ExoPowerShellModule = Get-ChildItem -Path $ClickOnceAppsPath -Recurse -Include 'Microsoft.Exchange.Management.ExoPowershellModule.manifest'
-            $ExoPowerShellModuleDll = Join-Path -Path $ExoPowerShellModule.Directory.FullName -ChildPath 'Microsoft.Exchange.Management.ExoPowerShellModule.dll'
-            $ExoPowerShellModulePs1 = Join-Path -Path $ExoPowerShellModule.Directory.FullName -ChildPath 'CreateExoPSSession.ps1'
-
-            if ($ExoPowerShellModule) {
-                Import-Module -FullyQualifiedName $ExoPowerShellModuleDll
-                . $ExoPowerShellModulePs1
-            } else {
-                throw 'Required module not available: Microsoft.Exchange.Management.ExoPowershellModule'
-            }
-        }
+        Import-ExoPowershellModule
     }
 
-    Write-Verbose -Message 'Connecting to Exchange Online ...'
+    Write-Host -ForegroundColor Green -Object 'Connecting to Exchange Online ...'
     if ($PSCmdlet.ParameterSetName -eq 'MFA') {
-        Connect-EXOPSSession @PSBoundParameters
+        Connect-EXOPSSession -UserPrincipalName $MfaUsername
     } else {
-        # Workaround for a weird bug
-        # See: https://stackoverflow.com/questions/41596482/import-pssession-error-when-called-in-a-function
-        Remove-Variable -Name UserPrincipalName
-
-        $ExchangeOnline = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri 'https://outlook.office365.com/powershell-liveid/' -Credential $Credential -Authentication 'Basic' -AllowRedirection
-        Import-PSSession -Session $ExchangeOnline -DisableNameChecking
+        $ExchangeOnline = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri 'https://outlook.office365.com/powershell-liveid/' -Credential $Credential -Authentication Basic -AllowRedirection
+        Import-PSSession -Session $ExchangeOnline
     }
 }
 
+Function Connect-SecurityAndComplianceCenter {
+    [CmdletBinding(DefaultParameterSetName='MFA')]
+    Param(
+        [Parameter(ParameterSetName='MFA')]
+        [ValidateNotNullOrEmpty()]
+        [String]$MfaUsername,
+
+        [Parameter(ParameterSetName='Standard',Mandatory)]
+        [ValidateNotNull()]
+        [System.Management.Automation.Credential()]
+        [PSCredential]$Credential
+    )
+
+    if ($PSCmdlet.ParameterSetName -eq 'MFA') {
+        Import-ExoPowershellModule
+    }
+
+    Write-Host -ForegroundColor Green -Object 'Connecting to Security and Compliance Center ...'
+    if ($PSCmdlet.ParameterSetName -eq 'MFA') {
+        Connect-IPPSSession -UserPrincipalName $MfaUsername
+    } else {
+        $SCC = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri 'https://ps.compliance.protection.outlook.com/powershell-liveid/' -Credential $Credential -Authentication Basic -AllowRedirection
+        Import-PSSession -Session $SCC
+    }
+}
 
 Function Connect-SharePointOnline {
+    [CmdletBinding()]
     Param(
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
@@ -88,7 +114,7 @@ Function Connect-SharePointOnline {
         throw 'Required module not available: Microsoft.Online.SharePoint.PowerShell'
     }
 
-    Write-Verbose -Message 'Connecting to SharePoint Online ...'
+    Write-Host -ForegroundColor Green -Object 'Connecting to SharePoint Online ...'
     $SPOUrl = 'https://{0}-admin.sharepoint.com' -f $TenantName
     if ($Credential) {
         Connect-SPOService -Url $SPOUrl -Credential $Credential
@@ -97,10 +123,14 @@ Function Connect-SharePointOnline {
     }
 }
 
-
 Function Connect-SkypeForBusinessOnline {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='MFA')]
     Param(
+        [Parameter(ParameterSetName='MFA')]
+        [ValidateNotNullOrEmpty()]
+        [String]$MfaUsername,
+
+        [Parameter(ParameterSetName='Standard',Mandatory)]
         [ValidateNotNull()]
         [System.Management.Automation.Credential()]
         [PSCredential]$Credential
@@ -110,30 +140,17 @@ Function Connect-SkypeForBusinessOnline {
         throw 'Required module not available: SkypeOnlineConnector'
     }
 
-    Write-Verbose -Message 'Connecting to Skype for Business Online ...'
-    $SkypeForBusinessOnline = New-CsOnlineSession @PSBoundParameters
-    Import-PSSession -Session $SkypeForBusinessOnline
+    Write-Host -ForegroundColor Green -Object 'Connecting to Skype for Business Online ...'
+    if ($PSCmdlet.ParameterSetName -eq 'MFA') {
+        $CsOnlineSession = New-CsOnlineSession -UserName $MfaUsername
+    } else {
+        $CsOnlineSession = New-CsOnlineSession -Credential $Credential
+    }
+    Import-PSSession -Session $CsOnlineSession
 }
-
-
-Function Connect-Office365SecurityAndComplianceCenter {
-    Param(
-        [Parameter(Mandatory)]
-        [ValidateNotNull()]
-        [System.Management.Automation.Credential()]
-        [PSCredential]$Credential,
-
-        [ValidateNotNullOrEmpty()]
-        [String]$CmdletsPrefix='Scc'
-    )
-
-    Write-Verbose -Message 'Connecting to Office 365 Security and Compliance Center ...'
-    $Office365SCC = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri 'https://ps.compliance.protection.outlook.com/powershell-liveid/' -Credential $Credential -Authentication 'Basic' -AllowRedirection
-    Import-PSSession -Session $Office365SCC -Prefix $CmdletsPrefix
-}
-
 
 Function Connect-Office365CentralizedDeployment {
+    [CmdletBinding()]
     Param(
         [Parameter(Mandatory)]
         [ValidateNotNull()]
@@ -145,6 +162,33 @@ Function Connect-Office365CentralizedDeployment {
         throw 'Required module not available: OrganizationAddInService'
     }
 
-    Write-Verbose -Message 'Connecting to Office 365 Centralized Deployment ...'
+    Write-Host -ForegroundColor Green -Object 'Connecting to Office 365 Centralized Deployment ...'
     Connect-OrganizationAddInService @PSBoundParameters
+}
+
+Function Import-ExoPowershellModule {
+    [CmdletBinding()]
+    Param()
+
+    if (!(Get-Command -Name Connect-EXOPSSession -ErrorAction Ignore)) {
+        $ClickOnceAppsPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath 'Apps\2.0'
+        $ExoPowerShellModule = Get-ChildItem -Path $ClickOnceAppsPath -Recurse -Include 'Microsoft.Exchange.Management.ExoPowershellModule.manifest'
+        $ExoPowerShellModuleDll = Join-Path -Path $ExoPowerShellModule.Directory -ChildPath 'Microsoft.Exchange.Management.ExoPowerShellModule.dll'
+        $ExoPowerShellModulePs1 = Join-Path -Path $ExoPowerShellModule.Directory -ChildPath 'CreateExoPSSession.ps1'
+
+        if ($ExoPowerShellModule) {
+            # Sourcing the script rudely changes the current working directory
+            $CurrentPath = Get-Location
+            . $ExoPowerShellModulePs1
+            Set-Location -Path $CurrentPath
+
+            # Change the scope of imported functions to be global (better approach?)
+            $Functions = @('Connect-EXOPSSession', 'Connect-IPPSSession', 'Test-Uri')
+            foreach ($Function in $Functions) {
+                $null = New-Item -Path Function: -Name global:$Function -Value (Get-Content -Path Function:\$Function)
+            }
+        } else {
+            throw 'Required module not available: Microsoft.Exchange.Management.ExoPowershellModule'
+        }
+    }
 }
