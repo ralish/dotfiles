c71f5d364ac913bc65f076f31430adde64bc71b6
Samuel D. Leslie <sdl@nexiom.net>
2017-11-22 16:38:16 +1100

    Major reworking of OpenSSH client config generation

--- metadata/openssh.sh
+++ metadata/openssh.sh
@@ -6,44 +6,41 @@ script_dir="$(dirname "${BASH_SOURCE[0]}")"
 source "$script_dir/templates/common.sh"
 
 if ! command -v ssh > /dev/null; then
-    exit $DETECTION_NOT_AVAILABLE
+    exit "$DETECTION_NOT_AVAILABLE"
 fi
 
-# Paths we need to know to build our configuration
+# Important paths
 OPENSSH_DIR="$script_dir/../openssh/.ssh"
 OPENSSH_CFG="$OPENSSH_DIR/config"
-TEMPLATE_DIR="$OPENSSH_DIR/templates"
-CUSTOM_CFG="$TEMPLATE_DIR/ssh_config"
+OPENSSH_INCLUDES="$OPENSSH_DIR/includes"
+OPENSSH_TEMPLATES="$OPENSSH_DIR/templates"
+OPENSSH_BANNER="$OPENSSH_TEMPLATES/banner"
 
-# Ensure the configuration has the correct permissions
-function set_config_perms() {
-    touch "$OPENSSH_CFG"
-    chmod 0600 "$OPENSSH_CFG"
-}
-
-# Check for a supported version and build our config
+# Check for a supported version
 ssh_version=$(ssh -V 2>&1)
 if [[ $ssh_version =~ OpenSSH_5\.9 ]]; then
-    set_config_perms
-    head -n -1 "$CUSTOM_CFG" > "$OPENSSH_CFG"
-    tail -n +4 "$TEMPLATE_DIR/ssh_config.59" >> "$OPENSSH_CFG"
+    ssh_template="$OPENSSH_TEMPLATES/ssh_config.59"
 elif [[ $ssh_version =~ OpenSSH_6\.6 ]]; then
-    set_config_perms
-    head -n -1 "$CUSTOM_CFG" > "$OPENSSH_CFG"
-    tail -n +4 "$TEMPLATE_DIR/ssh_config.66" >> "$OPENSSH_CFG"
+    ssh_template="$OPENSSH_TEMPLATES/ssh_config.66"
 elif [[ $ssh_version =~ OpenSSH_6\.9 ]]; then
-    set_config_perms
-    head -n -1 "$CUSTOM_CFG" > "$OPENSSH_CFG"
-    tail -n +4 "$TEMPLATE_DIR/ssh_config.69" >> "$OPENSSH_CFG"
+    ssh_template="$OPENSSH_TEMPLATES/ssh_config.69"
 elif [[ $ssh_version =~ OpenSSH_7\.2 ]]; then
-    set_config_perms
-    head -n -1 "$CUSTOM_CFG" > "$OPENSSH_CFG"
-    tail -n +4 "$TEMPLATE_DIR/ssh_config.72" >> "$OPENSSH_CFG"
+    ssh_template="$OPENSSH_TEMPLATES/ssh_config.72"
 else
-    echo "Unsupported OpenSSH version detected: $ssh_version"
-    exit $DETECTION_NO_LOGIC
+    printf 'Unsupported OpenSSH version: %s' "$ssh_version"
+    exit "$DETECTION_NO_LOGIC"
 fi
 
-exit $DETECTION_SUCCESS
+# Build our configuration
+echo -n > "$OPENSSH_CFG"
+chmod 0600 "$OPENSSH_CFG"
+head -n -1 "$OPENSSH_BANNER" >> "$OPENSSH_CFG"
+for include in $OPENSSH_INCLUDES/*; do
+    head -n -1 "$include" >> "$OPENSSH_CFG"
+    echo >> "$OPENSSH_CFG"
+done
+cat "$ssh_template" >> "$OPENSSH_CFG"
+
+exit "$DETECTION_SUCCESS"
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
