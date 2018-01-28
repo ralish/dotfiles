068349aa0ba3f8f66eda4de4c8edf515e3e5011c
Samuel D. Leslie <sdl@nexiom.net>
2018-01-29 09:55:21 +1100

    Add simple PoSh function to build our OpenSSH config

---
+++ posh/Functions/OpenSSH.ps1
@@ -0,0 +1,28 @@
+# Update our OpenSSH configuration
+Function Update-OpenSSHConfig {
+    [CmdletBinding()]
+    Param()
+
+    $BaseDir = Join-Path -Path $DotFilesPath -ChildPath 'openssh/.ssh'
+    $IncludesDir = Join-Path -Path $BaseDir -ChildPath 'includes'
+    $TemplatesDir = Join-Path -Path $BaseDir -ChildPath 'templates'
+
+    $ConfigFile = Join-Path -Path $BaseDir -ChildPath 'config'
+    $BannerFile = Join-Path -Path $TemplatesDir -ChildPath 'banner'
+    $TemplateFile = Join-Path -Path $TemplatesDir -ChildPath 'ssh_config.72'
+
+    # Make sure we create the file without a BOM
+    [IO.File]::WriteAllLines($ConfigFile, '')
+
+    $Banner = Get-Content -Path $BannerFile
+    Add-Content -Path $ConfigFile -Value $Banner[0..($Banner.Length - 2)]
+
+    $Includes = Get-ChildItem -Path $IncludesDir -File | Where-Object { $_.Length -gt 0 }
+    foreach ($Include in $Includes) {
+        $Data = Get-Content -Path $Include.FullName
+        Add-Content -Path $ConfigFile -Value $Data[0..($Data.Length - 2)]
+    }
+
+    $Template = Get-Content -Path $TemplateFile
+    Add-Content -Path $ConfigFile -Value $Template[0..($Template.Length - 2)]
+}
