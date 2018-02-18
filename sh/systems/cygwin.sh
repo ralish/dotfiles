1c69e9ef316e5b6692f985b93101131d7093022c
Samuel D. Leslie <sdl@nexiom.net>
2018-02-18 18:14:20 +1100

    Miscellaneous improvements to Cygwin shell config

--- sh/systems/cygwin.sh
+++ sh/systems/cygwin.sh
@@ -2,27 +2,26 @@
 
 # Cygwin shell configuration
 
-# Cygwin package repository mirrors to use
-CYGWIN_PKGS_MAIN='http://mirror.internode.on.net/pub/cygwin/'  # Cygwin Core
-CYGWIN_PKGS_PORTS='ftp://ftp.cygwinports.org/pub/cygwinports/' # Cygwin Ports
+# Cygwin package repository mirror
+CYGWIN_PKGS='http://mirrors.kernel.org/sourceware/cygwin/'
 
 # -----------------------------------------------------------------------------
 
-# Handy aliases to each package repository
+# Configure package repository for apt-cyg
 if command -v apt-cyg > /dev/null; then
-    alias apt-cyg="apt-cyg -m $CYGWIN_PKGS_MAIN"
-    alias apt-cyp="apt-cyg -m $CYGWIN_PKGS_PORTS"
+    # shellcheck disable=SC2139
+    alias apt-cyg="apt-cyg -m '$CYGWIN_PKGS'"
 fi
 
-# Because I never remember the '-s' parameter
-if command -v ssh-agent > /dev/null; then
-    alias ssh-agent-cyg='eval $(ssh-agent -s)'
-fi
-
-# Sort out the Git prompt (__git_ps1 may not be available)
+# Fix Git prompt (__git_ps1 may not be available)
 if command -v git-prompt > /dev/null; then
     # shellcheck source=/dev/null
     source "$(command -v git-prompt)"
+fi
+
+# Because I never remember the "-s" parameter
+if command -v ssh-agent > /dev/null; then
+    alias ssh-agent='eval $(ssh-agent -s)'
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
