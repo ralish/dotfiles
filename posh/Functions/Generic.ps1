1d84279bfcf065997c813a381e455051afb007e5
Samuel D. Leslie <sdl@nexiom.net>
2018-04-05 10:35:18 +1000

    Add PowerShell function to change text file encoding

--- posh/Functions/Generic.ps1
+++ posh/Functions/Generic.ps1
@@ -1,3 +1,29 @@
+# Convert a text file to the given encoding
+Function ConvertTo-TextEncoding {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [IO.FileInfo]$File,
+
+        [ValidateSet('ASCII', 'UTF7', 'UTF8', 'UTF16', 'UTF16BE', 'UTF32')]
+        [String]$Encoding='UTF8'
+    )
+
+    switch ($Encoding) {
+        ASCII       { $Encoder = [Text.Encoding]::ASCII }
+        UTF7        { $Encoder = [Text.Encoding]::UTF7 }
+        UTF8        { $Encoder = [Text.Encoding]::UTF8 }
+        UTF16       { $Encoder = [Text.Encoding]::Unicode }
+        UTF16BE     { $Encoder = [Text.Encoding]::BigEndianUnicode }
+        UTF32       { $Encoder = [Text.Encoding]::UTF32 }
+    }
+
+    $Content = Get-Content -Path $File
+
+    Write-Verbose -Message ('Converting to {0}: {1}' -f $Encoding, $File.Name)
+    [IO.File]::WriteAllLines($File.FullName, $Content, $Encoder)
+}
+
 # Compare the properties of two objects
 # Via: https://blogs.technet.microsoft.com/janesays/2017/04/25/compare-all-properties-of-two-objects-in-windows-powershell/
 Function Compare-ObjectProperties {
