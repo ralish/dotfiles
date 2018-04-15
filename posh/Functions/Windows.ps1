8b7f4e61f6236b06ea2e75f11a44a23cf542f854
Samuel D. Leslie <sdl@nexiom.net>
2018-04-15 14:11:18 +1000

    Move some Windows specific PoSh functions to Windows.ps1

--- posh/Functions/Windows.ps1
+++ posh/Functions/Windows.ps1
@@ -1,3 +1,23 @@
+# Watch an Event Log (similar to Unix "tail")
+# Slightly improved from: http://stackoverflow.com/questions/15262196/powershell-tail-windows-event-log-is-it-possible
+Function Get-EventLogTail {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [String]$EventLog
+    )
+
+    $IndexOld = (Get-EventLog -LogName $EventLog -Newest 1).Index
+    do {
+        Start-Sleep -Seconds 1
+        $IndexNew = (Get-EventLog -LogName $EventLog -Newest 1).Index
+        if ($IndexNew -ne $IndexOld) {
+            Get-EventLog -LogName $EventLog -Newest ($IndexNew - $IndexOld) | Sort-Object -Property Index
+            $IndexOld = $IndexNew
+        }
+    } while ($true)
+}
+
 # Retrieve files with a minimum number of hard links
 Function Get-MultipleHardLinks {
     [CmdletBinding()]
@@ -52,4 +72,9 @@ Function Get-NonInheritedACLs {
     }
 
     return $ACLMatches
+}
+
+# Helper function to call MKLINK in cmd
+Function mklink {
+    & $env:ComSpec /c mklink $args
 }
