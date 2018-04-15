db7849388eaa79b08ec5de71255ca4813ae7f911
Samuel D. Leslie <sdl@nexiom.net>
2018-04-15 18:57:37 +1000

    Rename Get-EventLogTail to Watch-EventLog

--- posh/Functions/Windows.ps1
+++ posh/Functions/Windows.ps1
@@ -1,23 +1,3 @@
-# Watch an Event Log (similar to Unix "tail")
-# Slightly improved from: http://stackoverflow.com/questions/15262196/powershell-tail-windows-event-log-is-it-possible
-Function Get-EventLogTail {
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory)]
-        [String]$EventLog
-    )
-
-    $IndexOld = (Get-EventLog -LogName $EventLog -Newest 1).Index
-    do {
-        Start-Sleep -Seconds 1
-        $IndexNew = (Get-EventLog -LogName $EventLog -Newest 1).Index
-        if ($IndexNew -ne $IndexOld) {
-            Get-EventLog -LogName $EventLog -Newest ($IndexNew - $IndexOld) | Sort-Object -Property Index
-            $IndexOld = $IndexNew
-        }
-    } while ($true)
-}
-
 # Retrieve files with a minimum number of hard links
 Function Get-MultipleHardLinks {
     [CmdletBinding()]
@@ -77,4 +57,24 @@ Function Get-NonInheritedACLs {
 # Helper function to call MKLINK in cmd
 Function mklink {
     & $env:ComSpec /c mklink $args
+}
+
+# Watch an Event Log (similar to Unix "tail")
+# Slightly improved from: http://stackoverflow.com/questions/15262196/powershell-tail-windows-event-log-is-it-possible
+Function Watch-EventLog {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [String]$EventLog
+    )
+
+    $IndexOld = (Get-EventLog -LogName $EventLog -Newest 1).Index
+    do {
+        Start-Sleep -Seconds 1
+        $IndexNew = (Get-EventLog -LogName $EventLog -Newest 1).Index
+        if ($IndexNew -ne $IndexOld) {
+            Get-EventLog -LogName $EventLog -Newest ($IndexNew - $IndexOld) | Sort-Object -Property Index
+            $IndexOld = $IndexNew
+        }
+    } while ($true)
 }
