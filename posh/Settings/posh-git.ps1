a2c17ba81a8b452fe190ebfd6183829c653b9fa7
Samuel D. Leslie <sdl@nexiom.net>
2018-04-20 08:37:03 +1000

    Some tweaks to our posh-git prompt

--- posh/Settings/posh-git.ps1
+++ posh/Settings/posh-git.ps1
@@ -1,5 +1,11 @@
 if (Get-Module -Name posh-git -ListAvailable) {
     Import-Module -Name posh-git
+
+    # Abbreviate home directory path with tilde
+    $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+
+    # Prefix prompt with username and hostname
+    $GitPromptSettings.DefaultPromptPrefix.Text = '{0}@{1} ' -f $env:USERNAME, $env:COMPUTERNAME
 } else {
     Write-Verbose -Message '[dotfiles] Skipping posh-git settings as module not found.'
 }
