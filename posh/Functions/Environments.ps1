e0d597053b1a4095f0f16abcc3ed4468bb3c510f
Samuel D. Leslie <sdl@nexiom.net>
2018-05-07 08:02:50 +1000

    Add enablement functions for PHP & Ruby + misc clean-up

--- posh/Functions/Environments.ps1
+++ posh/Functions/Environments.ps1
@@ -1,22 +1,30 @@
-# Prepend Node.js directories to PATH variable
+# Configure environment for Node.js development
 Function Enable-Nodejs {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory)]
-        [String]$Path
+        [ValidateNotNullOrEmpty()]
+        [String]$Path='C:\Nodejs'
     )
+
+    if (-not (Test-Path -Path $Path -PathType Container)) {
+        throw 'Provided Nodejs path is not a directory: {0}' -f $Path
+    }
 
     $LocalNpmPath = Join-Path -Path $Env:APPDATA -ChildPath 'npm'
     Set-Item -Path Env:\Path -Value ('{0};{1};{2}' -f $LocalNpmPath, $Path, $Env:PATH)
 }
 
-# Prepend Perl directories to PATH variable
+# Configure environment for Perl development
 Function Enable-Perl {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory)]
-        [String]$Path
+        [ValidateNotNullOrEmpty()]
+        [String]$Path='C:\Perl'
     )
+
+    if (-not (Test-Path -Path $Path -PathType Container)) {
+        throw 'Provided Perl path is not a directory: {0}' -f $Path
+    }
 
     $RootBinPath = Join-Path -Path $Path -ChildPath 'c\bin'
     $SiteBinPath = Join-Path -Path $Path -ChildPath 'perl\site\bin'
@@ -24,14 +32,55 @@ Function Enable-Perl {
     Set-Item -Path Env:\Path -Value ('{0};{1};{2};{3}' -f $RootBinPath, $SiteBinPath, $PerlBinPath, $Env:PATH)
 }
 
-# Prepend Python directories to PATH variable
+# Configure environment for PHP development
+Function Enable-PHP {
+    [CmdletBinding()]
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [String]$Path='C:\PHP'
+    )
+
+    if (-not (Test-Path -Path $Path -PathType Container)) {
+        throw 'Provided PHP path is not a directory: {0}' -f $Path
+    }
+
+    Set-Item -Path Env:\Path -Value ('{0};{1}' -f $Path, $Env:PATH)
+}
+
+# Configure environment for Python development
 Function Enable-Python {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory)]
-        [String]$Path
+        [ValidateNotNullOrEmpty()]
+        [String]$Path='C:\Python'
     )
+
+    if (-not (Test-Path -Path $Path -PathType Container)) {
+        throw 'Provided Python path is not a directory: {0}' -f $Path
+    }
 
     $ScriptsPath = Join-Path -Path $Path -ChildPath 'Scripts'
     Set-Item -Path Env:\Path -Value ('{0};{1};{2}' -f $ScriptsPath, $Path, $Env:PATH)
+}
+
+# Configure environment for Ruby development
+Function Enable-Ruby {
+    [CmdletBinding()]
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [String]$Path='C:\Ruby',
+
+        [String]$Options='-Eutf-8'
+    )
+
+    if (-not (Test-Path -Path $Path -PathType Container)) {
+        throw 'Provided Ruby path is not a directory: {0}' -f $Path
+    }
+
+    $BinPath = Join-Path -Path $Path -ChildPath 'bin'
+    Set-Item -Path Env:\Path -Value ('{0};{1}' -f $BinPath, $Env:PATH)
+
+    if ($Options) {
+        Set-Item -Path Env:\RUBYOPT -Value $Options
+    }
 }
