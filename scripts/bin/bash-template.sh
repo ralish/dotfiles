af5c2c5cd9f9803b23368298d9241735eb894263
Samuel D. Leslie <sdl@nexiom.net>
2018-07-15 12:02:19 +1000

    Update to latest bash-script-template source

--- scripts/bin/bash-template.sh
+++ scripts/bin/bash-template.sh
@@ -9,6 +9,7 @@
 
 # DESC: Handler for unexpected errors
 # ARGS: $1 (optional): Exit code (defaults to 1)
+# OUTS: None
 function script_trap_err() {
     local exit_code=1
 
@@ -20,7 +21,7 @@ function script_trap_err() {
     set +o pipefail
 
     # Validate any provided exit code
-    if [[ $# -eq 1 && $1 =~ ^[0-9]+$ ]]; then
+    if [[ ${1-} =~ ^[0-9]+$ ]]; then
         exit_code="$1"
     fi
 
@@ -55,12 +56,18 @@ function script_trap_err() {
 
 # DESC: Handler for exiting the script
 # ARGS: None
+# OUTS: None
 function script_trap_exit() {
     cd "$orig_cwd"
 
     # Remove Cron mode script log
     if [[ -n ${cron-} && -f ${script_output-} ]]; then
         rm "$script_output"
+    fi
+
+    # Remove script execution lock
+    if [[ -d ${script_lock-} ]]; then
+        rmdir "$script_lock"
     fi
 
     # Restore terminal colours
@@ -71,13 +78,14 @@ function script_trap_exit() {
 # DESC: Exit script with the given message
 # ARGS: $1 (required): Message to print on exit
 #       $2 (optional): Exit code (defaults to 0)
+# OUTS: None
 function script_exit() {
     if [[ $# -eq 1 ]]; then
         printf '%s\n' "$1"
         exit 0
     fi
 
-    if [[ $# -eq 2 && $2 =~ ^[0-9]+$ ]]; then
+    if [[ ${2-} =~ ^[0-9]+$ ]]; then
         printf '%b\n' "$1"
         # If we've been provided a non-zero exit code run the error trap
         if [[ $2 -ne 0 ]]; then
@@ -87,12 +95,22 @@ function script_exit() {
         fi
     fi
 
-    script_exit 'Invalid arguments passed to script_exit()!' 2
+    script_exit 'Missing required argument to script_exit()!' 2
 }
 
 
 # DESC: Generic script initialisation
 # ARGS: $@ (optional): Arguments provided to the script
+# OUTS: $orig_cwd: The current working directory when the script was run
+#       $script_path: The full path to the script
+#       $script_dir: The directory path of the script
+#       $script_name: The file name of the script
+#       $script_params: The original parameters provided to the script
+#       $ta_none: The ANSI control code to reset all text attributes
+# NOTE: $script_path only contains the path that was used to call the script
+#       and will not resolve any symlinks which may be present in the path.
+#       You can use a tool like realpath to obtain the "true" path. The same
+#       caveat applies to both the $script_dir and $script_name variables.
 function script_init() {
     # Useful paths
     readonly orig_cwd="$PWD"
@@ -108,6 +126,8 @@ function script_init() {
 
 # DESC: Initialise colour variables
 # ARGS: None
+# OUTS: Read-only variables with ANSI control codes
+# NOTE: If --no-colour was set the variables will be empty
 function colour_init() {
     if [[ -z ${no_colour-} ]]; then
         # Text attributes
@@ -190,6 +210,7 @@ function colour_init() {
 
 # DESC: Initialise Cron mode
 # ARGS: None
+# OUTS: $script_output: Path to the file stdout & stderr was redirected to
 function cron_init() {
     if [[ -n ${cron-} ]]; then
         # Redirect all output to a temporary file
@@ -199,14 +220,41 @@ function cron_init() {
 }
 
 
+# DESC: Acquire script lock
+# ARGS: $1 (optional): Scope of script execution lock (system or user)
+# OUTS: $script_lock: Path to the directory indicating we have the script lock
+# NOTE: This lock implementation is extremely simple but should be reliable
+#       across all platforms. It does *not* support locking a script with
+#       symlinks or multiple hardlinks as there's no portable way of doing so.
+#       If the lock was acquired it's automatically released on script exit.
+function lock_init() {
+    local lock_dir
+    if [[ $1 = 'system' ]]; then
+        lock_dir="/tmp/$script_name.lock"
+    elif [[ $1 = 'user' ]]; then
+        lock_dir="/tmp/$script_name.$UID.lock"
+    else
+        script_exit 'Missing or invalid argument to lock_init()!' 2
+    fi
+
+    if mkdir "$lock_dir" 2> /dev/null; then
+        readonly script_lock="$lock_dir"
+        verbose_print "Acquired script lock: $script_lock"
+    else
+        script_exit "Unable to acquire script lock: $lock_dir" 2
+    fi
+}
+
+
 # DESC: Pretty print the provided string
 # ARGS: $1 (required): Message to print (defaults to a green foreground)
 #       $2 (optional): Colour to print the message with. This can be an ANSI
 #                      escape code or one of the prepopulated colour variables.
 #       $3 (optional): Set to any value to not append a new line to the message
+# OUTS: None
 function pretty_print() {
-    if [[ $# -eq 0 || $# -gt 3 ]]; then
-        script_exit 'Invalid arguments passed to pretty_print()!' 2
+    if [[ $# -lt 1 ]]; then
+        script_exit 'Missing required argument to pretty_print()!' 2
     fi
 
     if [[ -z ${no_colour-} ]]; then
@@ -228,6 +276,7 @@ function pretty_print() {
 
 # DESC: Only pretty_print() the provided string if verbose mode is enabled
 # ARGS: $@ (required): Passed through to pretty_pretty() function
+# OUTS: None
 function verbose_print() {
     if [[ -n ${verbose-} ]]; then
         pretty_print "$@"
@@ -241,8 +290,8 @@ function verbose_print() {
 # OUTS: $build_path: The constructed path
 # NOTE: Heavily inspired by: https://unix.stackexchange.com/a/40973
 function build_path() {
-    if [[ -z ${1-} || $# -gt 2 ]]; then
-        script_exit 'Invalid arguments passed to build_path()!' 2
+    if [[ $# -lt 1 ]]; then
+        script_exit 'Missing required argument to build_path()!' 2
     fi
 
     local new_path path_entry temp_path
@@ -271,9 +320,10 @@ function build_path() {
 # DESC: Check a binary exists in the search path
 # ARGS: $1 (required): Name of the binary to test for existence
 #       $2 (optional): Set to any value to treat failure as a fatal error
+# OUTS: None
 function check_binary() {
-    if [[ $# -ne 1 && $# -ne 2 ]]; then
-        script_exit 'Invalid arguments passed to check_binary()!' 2
+    if [[ $# -lt 1 ]]; then
+        script_exit 'Missing required argument to check_binary()!' 2
     fi
 
     if ! command -v "$1" > /dev/null 2>&1; then
@@ -292,11 +342,8 @@ function check_binary() {
 
 # DESC: Validate we have superuser access as root (via sudo if requested)
 # ARGS: $1 (optional): Set to any value to not attempt root access via sudo
+# OUTS: None
 function check_superuser() {
-    if [[ $# -gt 1 ]]; then
-        script_exit 'Invalid arguments passed to check_superuser()!' 2
-    fi
-
     local superuser test_euid
     if [[ $EUID -eq 0 ]]; then
         superuser=true
@@ -328,15 +375,16 @@ function check_superuser() {
 # DESC: Run the requested command as root (via sudo if requested)
 # ARGS: $1 (optional): Set to zero to not attempt execution via sudo
 #       $@ (required): Passed through for execution as root user
+# OUTS: None
 function run_as_root() {
+    if [[ $# -eq 0 ]]; then
+        script_exit 'Missing required argument to run_as_root()!' 2
+    fi
+
     local try_sudo
     if [[ ${1-} =~ ^0$ ]]; then
         try_sudo=true
         shift
-    fi
-
-    if [[ $# -eq 0 ]]; then
-        script_exit 'Invalid arguments passed to run_as_root()!' 2
     fi
 
     if [[ $EUID -eq 0 ]]; then
