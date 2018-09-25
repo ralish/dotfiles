5f705f1bd95febcc230cfdbf045577b07c85546a
Samuel D. Leslie <sdl@nexiom.net>
2018-09-25 10:55:35 +1000

    Add .NET Core global tools to our path

--- sh/apps/dotnet.sh
+++ sh/apps/dotnet.sh
@@ -2,8 +2,17 @@
 
 # dotnet configuration
 if command -v dotnet > /dev/null; then
+    dotnet_bin="$HOME/.dotnet/tools"
+
     # Opt-out of telemetry
     export DOTNET_CLI_TELEMETRY_OPTOUT=true
+
+    # Add global tools to our PATH
+    build_path "$dotnet_bin" "$PATH"
+    # shellcheck disable=SC2154
+    export PATH="$build_path"
+
+    unset dotnet_bin
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
