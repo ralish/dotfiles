2f35cc1dad8ef23ebb2f0cfe4a0a919aa7936478
Samuel D. Leslie <sdl@nexiom.net>
2018-12-19 13:51:25 +1100

    Add PSReadline handler for inserting paired parenthesis

--- posh/Settings/PSReadLine.ps1
+++ posh/Settings/PSReadLine.ps1
@@ -12,6 +12,33 @@ if (Get-Module -Name PSReadLine -ListAvailable) {
 
         # Menu style command completion
         Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+        # Insert paired parenthesis on line or selection
+        # Via: https://github.com/lzybkr/PSReadLine/blob/master/PSReadLine/SamplePSReadLineProfile.ps1
+        Set-PSReadlineKeyHandler `
+            -Chord 'Alt+(' `
+            -BriefDescription InsertPairedParenthesis `
+            -LongDescription 'Insert parenthesis around the selection or the entire line if no text is selected' `
+            -ScriptBlock {
+                Param($Key, $Arg)
+
+                $Line = $null
+                $Cursor = $null
+                [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$Line, [ref]$Cursor)
+
+                $SelectionStart = $null
+                $SelectionLength = $null
+                [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$SelectionStart, [ref]$SelectionLength)
+
+                if ($SelectionStart -ne -1) {
+                    $SelectionText = $Line.SubString($SelectionStart, $SelectionLength)
+                    [Microsoft.PowerShell.PSConsoleReadLine]::Replace($SelectionStart, $SelectionLength, '({0})' -f $SelectionText)
+                    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($SelectionStart + $SelectionLength + 2)
+                } else {
+                    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(0, $Line.Length, '({0})' -f $Line)
+                    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+                }
+            }
     } else {
         Write-Verbose -Message '[dotfiles] Skipping PSReadLine settings as host is not ConsoleHost.'
     }
