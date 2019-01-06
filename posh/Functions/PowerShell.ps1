6bcde186ed6264fc4495a53294f542869a46f40e
Samuel D. Leslie <sdl@nexiom.net>
2019-01-06 15:25:52 +1100

    Make Uninstall-ObsoleteModule much smarter & faster

--- posh/Functions/PowerShell.ps1
+++ posh/Functions/PowerShell.ps1
@@ -49,17 +49,30 @@ Function Uninstall-ObsoleteModule {
         [String[]]$Name
     )
 
+    $GetParams = @{}
     if ($PSBoundParameters.ContainsKey('Name')) {
-        $Modules = Get-InstalledModule -Name $Name
-    } else {
-        $Modules = Get-InstalledModule
+        $GetParams['Name'] = $Name
     }
 
-    foreach ($Module in $Modules) {
+    Write-Verbose -Message 'Retrieving installed modules ...'
+    $InstalledModules = Get-InstalledModule -Verbose:$false @GetParams
+    Write-Verbose -Message 'Retrieving available modules ...'
+    $AvailableModules = Get-Module -ListAvailable -Verbose:$false @GetParams
+
+    foreach ($Module in $InstalledModules) {
+        # Try to avoid subsequent call to Get-InstalledModule as it's obscenely slow
+        [PSModuleInfo[]]$MatchingModules = $AvailableModules | Where-Object Name -eq $Module.Name
+        if ($MatchingModules.Count -eq 1) {
+            continue
+        }
+
         $AllVersions = Get-InstalledModule -AllVersions -Name $Module.Name
 
         if ($AllVersions.Count -gt 1) {
-            $AllVersions | Where-Object Version -ne $Module.Version | Uninstall-Module
+            Write-Verbose -Message ('Uninstalling {0} version(s): {1}' -f $Module.Name, [String]::Join(', ', $AllVersions.Version -ne $Module.Version))
+            if ($PSCmdlet.ShouldProcess($Module.Name, 'Uninstall obsolete versions')) {
+                $AllVersions | Where-Object Version -ne $Module.Version | Uninstall-Module
+            }
         }
     }
 }
