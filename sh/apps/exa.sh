f933b09b032514c02677258aa7c62fed64aaffc6
Samuel D. Leslie <sdl@nexiom.net>
2019-12-05 13:26:37 +1100

    Add aliases for exa

---
+++ sh/apps/exa.sh
@@ -0,0 +1,18 @@
+# shellcheck shell=sh
+
+# exa configuration
+if command -v exa > /dev/null; then
+    alias l='exa --binary'
+
+    alias la='exa --binary --all'
+    alias lg='exa --binary --git --long'
+    alias lr='exa --binary --recurse'
+    alias lt='exa --binary --tree'
+
+    alias ll='exa --binary --long'
+    alias lla='exa --binary --long --all'
+    alias llr='exa --binary --long --recurse'
+    alias llt='exa --binary --long --tree'
+fi
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
