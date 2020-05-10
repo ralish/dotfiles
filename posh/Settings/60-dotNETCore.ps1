3648701a67e8c122625062baae417bcaf3d8fec0
Samuel D. Leslie <sdl@nexiom.net>
2020-05-11 09:28:14 +1000

    Fix .NET Core completion support in PowerShell

--- posh/Settings/60-dotNETCore.ps1
+++ posh/Settings/60-dotNETCore.ps1
@@ -8,11 +8,11 @@ Write-Verbose -Message (Get-DotFilesMessage -Message 'Loading .NET Core settings
 # Opt-out of telemetry
 Set-Item -Path Env:\DOTNET_CLI_TELEMETRY_OPTOUT -Value 'true'
 
-# Command completion for the dotnet CLI
+# How to enable TAB completion for the .NET Core CLI
 # https://docs.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    Param($CommandName, $WordToComplete, $CursorPosition)
-    dotnet complete --position $CursorPosition, "$WordToComplete" | ForEach-Object {
+    Param($wordToComplete, $commandAst, $cursorPosition)
+    dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
         [Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
