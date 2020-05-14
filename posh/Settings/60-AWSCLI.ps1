2be05ff7e540d9a5eb42ba96f91b57ef231354b9
Samuel D. Leslie <sdl@nexiom.net>
2020-05-15 09:54:32 +1000

    Explicitly check for aws_completer in PowerShell

--- posh/Settings/60-AWSCLI.ps1
+++ posh/Settings/60-AWSCLI.ps1
@@ -7,12 +7,16 @@ Write-Verbose -Message (Get-DotFilesMessage -Message 'Loading AWS CLI settings .
 
 # Configuring the AWS CLI - Command Completion
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-completion.html
-Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
-    Param($wordToComplete, $commandAst, $cursorPosition)
-    $env:COMP_LINE = $commandAst.ToString()
-    $env:COMP_POINT = $cursorPosition
-    aws_completer | ForEach-Object {
-        [Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+if (Get-Command -Name aws_completer -ErrorAction Ignore) {
+    Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
+        Param($wordToComplete, $commandAst, $cursorPosition)
+        $env:COMP_LINE = $commandAst.ToString()
+        $env:COMP_POINT = $cursorPosition
+        aws_completer | ForEach-Object {
+            [Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+        $env:COMP_LINE = $env:COMP_POINT = $null
     }
-    $env:COMP_LINE = $env:COMP_POINT = $null
+} else {
+    Write-Warning -Message (Get-DotFilesMessage -Message 'Skipping AWS CLI completion as unable to locate aws_completer.')
 }
