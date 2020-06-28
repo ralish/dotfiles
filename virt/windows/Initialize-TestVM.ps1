41319a59cd99542666270b40541f37b6f51b933c
Samuel D. Leslie <sdl@nexiom.net>
2020-06-28 17:29:02 +1000

    Major updates to Initialize-TestVM.ps1

--- virt/windows/Initialize-TestVM.ps1
+++ virt/windows/Initialize-TestVM.ps1
@@ -1,5 +1,81 @@
 #Requires -RunAsAdministrator
 
+[CmdletBinding(DefaultParameterSetName = 'OptOut')]
+Param(
+    [Parameter(ParameterSetName = 'OptOut')]
+    [ValidateSet(
+        'DotNet',
+        'Office365',
+        'PowerShell',
+        'WindowsComponents',
+        'WindowsDefender',
+        'WindowsFeatures',
+        'WindowsSecurity',
+        'WindowsSettingsComputer',
+        'WindowsSettingsUser',
+        'WindowsUpdate'
+    )]
+    [String[]]$ExcludeTasks,
+
+    [Parameter(ParameterSetName = 'OptIn', Mandatory)]
+    [ValidateSet(
+        'DotNet',
+        'Office365',
+        'PowerShell',
+        'WindowsComponents',
+        'WindowsDefender',
+        'WindowsFeatures',
+        'WindowsSecurity',
+        'WindowsSettingsComputer',
+        'WindowsSettingsUser',
+        'WindowsUpdate'
+    )]
+    [String[]]$IncludeTasks
+)
+
+Function Optimize-DotNet {
+    [CmdletBinding()]
+    Param()
+
+    Test-DotNetPresent
+
+    if ($Script:DotNet20Present) {
+        Write-Host -ForegroundColor Green '[DotNet] Applying .NET Framework 2.x/3.x settings ...'
+
+        # Enable strong cryptography
+        Set-RegistryValue -Path 'HKLM:\Software\Microsoft\.NETFramework\v2.0.50727' -Name 'SchUseStrongCrypto' -Type DWord -Value 1
+        if ($Script:Wow64Present) {
+            Set-RegistryValue -Path 'HKLM:\Software\Wow6432Node\Microsoft\.NETFramework\v2.0.50727' -Name 'SchUseStrongCrypto' -Type DWord -Value 1
+        }
+
+        # Let OS choose protocols
+        Set-RegistryValue -Path 'HKLM:\Software\Microsoft\.NETFramework\v2.0.50727' -Name 'SystemDefaultTlsVersions' -Type DWord -Value 1 # DevSkim: ignore DS440000
+        if ($Script:Wow64Present) {
+            Set-RegistryValue -Path 'HKLM:\Software\Microsoft\.NETFramework\v2.0.50727' -Name 'SystemDefaultTlsVersions' -Type DWord -Value 1 # DevSkim: ignore DS440000
+        }
+    } else {
+        Write-Warning -Message 'Skipping .NET Framework 2.x/3.x settings as not installed.'
+    }
+
+    if ($Script:DotNet40Present) {
+        Write-Host -ForegroundColor Green '[DotNet] Applying .NET Framework 4.x settings ...'
+
+        # Enable strong cryptography
+        Set-RegistryValue -Path 'HKLM:\Software\Microsoft\.NETFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Type DWord -Value 1
+        if ($Script:Wow64Present) {
+            Set-RegistryValue -Path 'HKLM:\Software\Wow6432Node\Microsoft\.NETFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Type DWord -Value 1
+        }
+
+        # Let OS choose protocols
+        Set-RegistryValue -Path 'HKLM:\Software\Microsoft\.NETFramework\v4.0.30319' -Name 'SystemDefaultTlsVersions' -Type DWord -Value 1 # DevSkim: ignore DS440000
+        if ($Script:Wow64Present) {
+            Set-RegistryValue -Path 'HKLM:\Software\Microsoft\.NETFramework\v4.0.30319' -Name 'SystemDefaultTlsVersions' -Type DWord -Value 1 # DevSkim: ignore DS440000
+        }
+    } else {
+        Write-Warning -Message 'Skipping .NET Framework 4.x settings as not installed.'
+    }
+}
+
 Function Optimize-Office365 {
     [CmdletBinding()]
     Param()
@@ -17,11 +93,8 @@ Function Optimize-PowerShell {
         return
     }
 
-    Write-Host -ForegroundColor Green '[PowerShell] Determining modules to install ...'
-    $Modules = @('SpeculationControl', 'PSWindowsUpdate', 'PSWinGlue', 'PSWinVitals')
-    if (!(Get-Module -Name PSReadLine)) {
-        $Modules += 'PSReadLine'
-    }
+    Write-Host -ForegroundColor Green '[PowerShell] Setting PSGallery repository to trusted ...'
+    $null = Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
     Write-Host -ForegroundColor Green '[PowerShell] Installing NuGet package provider ...'
     $null = Install-PackageProvider -Name NuGet -Force
@@ -30,9 +103,15 @@ Function Optimize-PowerShell {
     $null = Install-Module -Name PowerShellGet -Force
     Import-Module -Name PowerShellGet -Force
 
+    Write-Host -ForegroundColor Green '[PowerShell] Determining modules to install ...'
+    $Modules = @('SpeculationControl', 'PSWindowsUpdate', 'PSWinGlue', 'PSWinVitals')
+    if (!(Get-Module -Name PSReadLine)) {
+        $Modules += 'PSReadLine'
+    }
+
     Write-Host -ForegroundColor Green '[PowerShell] Installing modules ...'
     foreach ($Module in $Modules) {
-        Write-Host -ForegroundColor Green ('[PowerShell] - {0}' -f $Module)
+        Write-Host -ForegroundColor Grey ('[PowerShell] - {0}' -f $Module)
         $null = Install-Module -Name $Module -Force
     }
 
@@ -84,9 +163,11 @@ Function Optimize-WindowsFeatures {
     Write-Host
 }
 
-Function Optimize-WindowsSettings {
+Function Optimize-WindowsSecurity {
     [CmdletBinding()]
     Param()
+
+    Test-Wow64Present
 
     Write-Host -ForegroundColor Green '[Windows] Applying security policy ...'
     $SecEditDb = Join-Path $env:windir 'Security\Local.sdb'
@@ -109,25 +190,50 @@ Function Optimize-WindowsSettings {
     Write-Host -ForegroundColor Gray '[SecEdit] - Cleaning-up ...'
     Remove-Item $SecEditCfg
 
-    Write-Host -ForegroundColor Green '[Windows] Applying system settings ...'
+    Write-Host -ForegroundColor Green '[Windows] Applying security settings ...'
+
+    # Set WinHTTP default protocols to: TLS 1.0, TLS 1.1, TLS 1.2
+    Set-RegistryValue -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -Name 'DefaultSecureProtocols' -Type DWord -Value 2688
+    if ($Script:Wow64Present) {
+        Set-RegistryValue -Path 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -Name 'DefaultSecureProtocols' -Type DWord -Value 2688
+    }
+}
+
+Function Optimize-WindowsSettingsComputer {
+    [CmdletBinding()]
+    Param()
+
+    Write-Host -ForegroundColor Green '[Windows] Applying computer settings ...'
+
     # Disable automatic maintenance
     Set-RegistryValue -Path 'HKLM:\Software\Policies\Microsoft\Windows\ScheduledDiagnostics' -Name 'EnabledExecution' -Type DWord -Value 0
+
     # Do not display Server Manager automatically at logon
     Set-RegistryValue -Path 'HKLM:\Software\Policies\Microsoft\Windows\Server\ServerManager' -Name 'DoNotOpenAtLogon' -Type DWord -Value 1
+
     # Disable Explorer SmartScreen
     Set-RegistryValue -Path 'HKLM:\Software\Policies\Microsoft\Windows\System' -Name 'EnableSmartScreen' -Type DWord -Value 0
+
     # Disable Shutdown Event Tracker
     Set-RegistryValue -Path 'HKLM:\Software\Policies\Microsoft\Windows NT\Reliability' -Name 'ShutdownReasonOn' -Type DWord -Value 0
+}
+
+Function Optimize-WindowsSettingsUser {
+    [CmdletBinding()]
+    Param()
 
     Write-Host -ForegroundColor Green '[Windows] Applying user settings ...'
+
     # Remove volume control icon
     $AudioSrv = Get-Service -Name AudioSrv -ErrorAction SilentlyContinue
     if ($AudioSrv.StartType -eq 'Disabled') {
         Set-RegistryValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer' -Name 'HideSCAVolume' -Type DWord -Value 1
     }
+
     # Remove Recycle Bin desktop icon
     Set-RegistryValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu' -Name '{645FF040-5081-101B-9F08-00AA002F954E}' -Type DWord -Value 1
     Set-RegistryValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel' -Name '{645FF040-5081-101B-9F08-00AA002F954E}' -Type DWord -Value 1
+
     # Disable startup programs launch delay
     Set-RegistryValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize' -Name 'StartupDelayInMSec' -Type DWord -Value 0
 }
@@ -183,10 +289,60 @@ Function Set-RegistryValue {
     }
 }
 
-Optimize-WindowsUpdate
-Optimize-WindowsDefender
-Optimize-WindowsSettings
-Optimize-WindowsFeatures
-Optimize-WindowsComponents
-Optimize-Office365
-Optimize-PowerShell
+Function Test-DotNetPresent {
+    [CmdletBinding()]
+    Param()
+
+    $ClrVersions = '2.0', '4.0'
+    foreach ($ClrVersion in $ClrVersions) {
+        $VarName = 'DotNet{0}Present' -f $ClrVersion.Replace('.', '')
+        switch ($ClrVersion) {
+            '2.0' { $RegPath = 'HKLM:\Software\Microsoft\NET Framework Setup\NDP\v2.0.50727' }
+            '4.0' { $RegPath = 'HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full' }
+        }
+
+        $RegKey = Get-Item -Path $RegPath -ErrorAction Ignore
+        if ($RegKey -and $RegKey.GetValue('Version')) {
+            Set-Variable -Name $VarName -Scope Script -Value $true
+        } else {
+            Set-Variable -Name $VarName -Scope Script -Value $false
+        }
+    }
+}
+
+Function Test-Wow64Present {
+    [CmdletBinding()]
+    Param()
+
+    if (Test-Path -Path 'HKLM:\Software\Wow6432Node\Microsoft\Windows NT\CurrentVersion' -PathType Container) {
+        $Script:Wow64Present = $true
+    } else {
+        $Script:Wow64Present = $false
+    }
+}
+
+$Tasks = @(
+    'WindowsUpdate',
+    'WindowsDefender',
+    'WindowsSecurity',
+    'WindowsSettingsComputer',
+    'WindowsSettingsUser',
+    'WindowsFeatures',
+    'WindowsComponents',
+    'DotNet',
+    'PowerShell',
+    'Office365'
+)
+
+foreach ($Task in $Tasks) {
+    $Function = 'Optimize-{0}' -f $Task
+    if ($PSCmdlet.ParameterSetName -eq 'OptOut') {
+        if ($ExcludeTasks -notcontains $Task) {
+            & $Function
+        }
+    } else {
+        if ($IncludeTasks -contains $Task) {
+            & $Function
+        }
+    }
+}
