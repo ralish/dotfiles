1af685649bd7579ea28f46a95c021037ac2b1452
Samuel D. Leslie <sdl@nexiom.net>
2020-07-12 16:12:19 +1000

    Check for existence of pip2 or pip3 in metadata

---
+++ metadata/pip.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Source in common metadata functions
+script_dir="$(dirname "${BASH_SOURCE[0]}")"
+# shellcheck source=metadata/templates/common.sh
+source "$script_dir/templates/common.sh"
+
+if ! command -v pip > /dev/null &&
+    ! command -v pip2 > /dev/null &&
+    ! command -v pip3 > /dev/null; then
+    exit $DETECTION_NOT_AVAILABLE
+fi
+
+exit $DETECTION_SUCCESS
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
