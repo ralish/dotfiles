da309eac5c1c726b8a99503917b4c2f193726d07
Samuel D. Leslie <sdl@nexiom.net>
2020-09-18 10:51:57 +1000

    Add Set-Paint3DContextMenu script

---
+++ win10/Set-Paint3DContextMenu.ps1
@@ -0,0 +1,43 @@
+#Requires -RunAsAdministrator
+
+[CmdletBinding(SupportsShouldProcess)]
+Param(
+    [ValidateSet('Enable', 'Disable')]
+    [String]$Operation,
+
+    [ValidateNotNullOrEmpty()]
+    [String[]]$Extensions = @('3mf', 'bmp', 'fbx', 'gif', 'glb', 'jfif', 'jpe', 'jpeg', 'jpg', 'obj', 'ply', 'png', 'stl' , 'tif', 'tiff')
+)
+
+$RemoveHKCRDrive = $false
+if (!(Get-PSDrive -Name HKCR -ErrorAction Ignore)) {
+    $RemoveHKCRDrive = $true
+    $null = New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+}
+
+foreach ($Extension in $Extensions) {
+    $Extension = $Extension.ToLower()
+    $ContextMenuPath = 'HKCR:\SystemFileAssociations\.{0}\Shell\3D Edit' -f $Extension
+
+    try {
+        $null = Get-ItemProperty -Path $ContextMenuPath -ErrorAction Stop
+    } catch {
+        Write-Warning -Message ('Skipping extension due to missing registry key: {0}' -f $Extension)
+        continue
+    }
+
+    if ($Operation -eq 'Enable') {
+        if ($PSCmdlet.ShouldProcess($Extension, 'Enable Paint 3D extension context menu')) {
+            Remove-ItemProperty -Path $ContextMenuPath -Name LegacyDisable -ErrorAction Ignore
+        }
+
+    } else {
+        if ($PSCmdlet.ShouldProcess($Extension, 'Disable Paint 3D extension context menu')) {
+            Set-ItemProperty -Path $ContextMenuPath -Name LegacyDisable -Value $null
+        }
+    }
+}
+
+if ($RemoveHKCRDrive) {
+    Remove-PSDrive -Name HKCR
+}
