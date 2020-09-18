723dcc83ad7b9d76e147c59090f1b45becb458db
Samuel D. Leslie <sdl@nexiom.net>
2020-09-18 10:52:10 +1000

    Add Set-SkypeContextMenu script

---
+++ win10/Set-SkypeContextMenu.ps1
@@ -0,0 +1,33 @@
+#Requires -RunAsAdministrator
+
+[CmdletBinding(SupportsShouldProcess)]
+Param(
+    [ValidateSet('Enable', 'Disable')]
+    [String]$Operation
+)
+
+$ContextMenuPath = 'HKLM:\Software\Classes\PackagedCom\Package\Microsoft.SkypeApp_*\Class\{776DBC8D-7347-478C-8D71-791E12EF49D8}'
+
+try {
+    $ContextMenuKey = Get-ItemProperty -Path $ContextMenuPath -ErrorAction Stop
+} catch {
+    throw 'Failed to retrieve registry key for Skype context menu.'
+}
+
+if (!$ContextMenuKey.DllPath) {
+    throw 'DllPath is empty or not-present for Skype context menu registry key.'
+}
+
+if ($Operation -eq 'Enable') {
+    if ($PSCmdlet.ShouldProcess('Enable Skype context menu')) {
+        if ($ContextMenuKey.DllPath.StartsWith('-')) {
+            Set-ItemProperty -Path $ContextMenuPath -Name DllPath -Value ('{0}' -f $ContextMenuKey.DllPath.Substring(1))
+        }
+    }
+} else {
+    if ($PSCmdlet.ShouldProcess('Disable Skype context menu')) {
+        if (!$ContextMenuKey.DllPath.StartsWith('-')) {
+            Set-ItemProperty -Path $ContextMenuPath -Name DllPath -Value ('-{0}' -f $ContextMenuKey.DllPath)
+        }
+    }
+}
