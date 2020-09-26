4f9cb633e3bf8008af1f4bb63f945a06a0c4ddc2
Samuel D. Leslie <sdl@nexiom.net>
2020-09-26 18:25:41 +1000

    Add PowerShell completion support for dotnet-suggest

---
+++ posh/Settings/61-dotnet-suggest.ps1
@@ -0,0 +1,22 @@
+if (!(Get-Command -Name dotnet-suggest -ErrorAction Ignore)) {
+    Write-Verbose -Message (Get-DotFilesMessage -Message 'Skipping dotnet-suggest settings as unable to locate dotnet-suggest.')
+    return
+}
+
+Write-Verbose -Message (Get-DotFilesMessage -Message 'Loading dotnet-suggest settings ...')
+
+# dotnet-suggest
+# https://github.com/dotnet/command-line-api/wiki/dotnet-suggest
+$env:DOTNET_SUGGEST_SCRIPT_VERSION = '1.0.0'
+
+$RegisteredApps = (dotnet-suggest list | Out-String).Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
+Register-ArgumentCompleter -Native -CommandName $RegisteredApps -ScriptBlock {
+    Param($wordToComplete, $commandAst, $cursorPosition)
+    $CommandPath = (Get-Command -Name $commandAst.CommandElements[0]).Source
+    $CommandArgs = $CommandAst.Extent.ToString().Replace('"', '\"')
+    dotnet-suggest get -e $CommandPath --position $cursorPosition -- $CommandArgs | ForEach-Object {
+        [Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
+Remove-Variable -Name 'RegisteredApps'
