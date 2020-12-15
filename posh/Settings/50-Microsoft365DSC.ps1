9965c97c59591c81cab31bcd70f505391f6f1ade
Samuel D. Leslie <sdl@nexiom.net>
2020-12-15 13:08:49 +1100

    Add Microsoft365DSC module

---
+++ posh/Settings/50-Microsoft365DSC.ps1
@@ -0,0 +1,16 @@
+try {
+    if (!$DotFilesFastLoad) {
+        Test-ModuleAvailable -Name Microsoft365DSC -Verbose:$false
+    }
+} catch {
+    Write-Verbose -Message (Get-DotFilesMessage -Message 'Skipping Microsoft365DSC settings as module not found.')
+    return
+}
+
+Write-Verbose -Message (Get-DotFilesMessage -Message 'Loading Microsoft365DSC settings ...')
+
+# Opt-out of telemetry
+$TelemetryStatus = Get-EnvironmentVariable -Name M365DSCTelemetryEnabled -Scope Machine
+if ($TelemetryStatus -ne 'False') {
+    Write-Warning -Message 'Telemetry is enabled for the Microsoft365DSC module.'
+}
