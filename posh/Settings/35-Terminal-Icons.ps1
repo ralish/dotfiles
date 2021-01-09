dcc2441cf7f08dd4988d1d5d8de8ac1357a76ef0
Samuel D. Leslie <sdl@nexiom.net>
2021-01-09 19:39:08 +1100

    Suppress verbose output on importing Terminal-Icons

--- posh/Settings/35-Terminal-Icons.ps1
+++ posh/Settings/35-Terminal-Icons.ps1
@@ -1,8 +1,19 @@
 try {
+    # Terminal-Icons runs some commands which don't respect the -Verbose
+    # parameter. Suppress them via $VerbosePreference before import.
+    if ($DotFilesVerbose) {
+        $VerbosePreference = 'SilentlyContinue'
+    }
+
     Import-Module -Name Terminal-Icons -ErrorAction Stop -Verbose:$false
 } catch {
     Write-Verbose -Message (Get-DotFilesMessage -Message 'Skipping Terminal-Icons settings as module not found.')
     return
+} finally {
+    # Restore the original $VerbosePreference setting
+    if ($DotFilesVerbose) {
+        $VerbosePreference = 'Continue'
+    }
 }
 
 Write-Verbose -Message (Get-DotFilesMessage -Message 'Loading Terminal-Icons settings ...')
