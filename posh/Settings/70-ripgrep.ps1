57beb0d54faa308767fbe5f0a22ecd5daf8bf336
Samuel D. Leslie <sdl@nexiom.net>
2021-01-21 14:09:50 +1100

    Only load ripgrep PowerShell completions on Windows

--- posh/Settings/70-ripgrep.ps1
+++ posh/Settings/70-ripgrep.ps1
@@ -12,22 +12,24 @@ Write-Verbose -Message (Get-DotFilesMessage -Message 'Loading ripgrep settings .
 # Path to our ripgrep configuration
 $env:RIPGREP_CONFIG_PATH = Join-Path -Path $HOME -ChildPath '.ripgreprc'
 
-# Attempt to load ripgrep completion
-try {
-    $RgCommand = Get-Command -Name rg
-    $RgCompletion = [String]::Empty
+# Attempt to load ripgrep completion (Windows only)
+if (Test-IsWindows) {
+    try {
+        $RgCommand = Get-Command -Name rg
+        $RgCompletion = [String]::Empty
 
-    if ($RgCommand.Name.EndsWith('.ps1')) {
-        $RgScriptPath = Get-Item -LiteralPath $RgCommand.Path
+        if ($RgCommand.Name.EndsWith('.ps1')) {
+            $RgScriptPath = Get-Item -LiteralPath $RgCommand.Path
 
-        if ($RgScriptPath.Directory.Name -eq 'shims') {
-            $RgCompletion = Join-Path -Path $RgScriptPath.Directory.Parent.FullName -ChildPath 'apps\ripgrep\current\complete\_rg.ps1'
+            if ($RgScriptPath.Directory.Name -eq 'shims') {
+                $RgCompletion = Join-Path -Path $RgScriptPath.Directory.Parent.FullName -ChildPath 'apps\ripgrep\current\complete\_rg.ps1'
+            }
         }
-    }
 
-    . $RgCompletion
-} catch {
-    Write-Warning -Message (Get-DotFilesMessage -Message 'Skipping ripgrep completion as unable to locate _rg.ps1.')
-} finally {
-    Remove-Variable -Name 'RgCommand', 'RgCompletion', 'RgScriptPath' -ErrorAction Ignore
+        . $RgCompletion
+    } catch {
+        Write-Warning -Message (Get-DotFilesMessage -Message 'Skipping ripgrep completion as unable to locate _rg.ps1.')
+    } finally {
+        Remove-Variable -Name 'RgCommand', 'RgCompletion', 'RgScriptPath' -ErrorAction Ignore
+    }
 }
