bad5eddb6fcee1d0e02fe2420bd28cf8d87b9dc2
Samuel D. Leslie <sdl@nexiom.net>
2021-03-14 19:30:54 +1100

    More extensive ignored properties for AD user object comparisons

--- posh/Settings/50-ActiveDirectory.ps1
+++ posh/Settings/50-ActiveDirectory.ps1
@@ -17,53 +17,73 @@ try {
 
 Write-Verbose -Message (Get-DotFilesMessage -Message 'Loading ActiveDirectory settings ...')
 
-# User properties we usually don't care about
+# AD class properties we may want to ignore: top
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignment', '')]
-$ADUserIgnoredProperties = @(
+$ADClassIgnoredPropertiesTop = @(
     # Only exposed via LDAP attributes
-    'dSCorePropagationData',
-    'lastLogoff',
-    'lockoutTime',
-    'logonCount',
-    'mS-DS-ConsistencyGuid',
-    'msDS-KeyCredentialLink',
-    'uSNChanged',
-    'uSNCreated',
+    'dSCorePropagationData'
+    'mS-DS-ConsistencyGuid'
+    'uSNChanged'
+    'uSNCreated'
+    'whenChanged'
+    'whenCreated'
 
-    # LDAP attributes & PoSh properties with identical names
-    'CanonicalName',
-    'CN',
-    'DistinguishedName',
-    'MemberOf',
-    'ObjectGUID',
+    # PoSh property followed by equivalent LDAP attribute
+    'Created', 'createTimeStamp'
+    'Modified', 'modifyTimeStamp'
 
-    # PoSh property followed by equivalent LDAP attribute(s)
-    'BadLogonCount',
-    'badPwdCount',
-
-    'Created',
-    'whenCreated',
-    'createTimeStamp',
-
-    'LastBadPasswordAttempt',
-    'badPasswordTime',
-
-    'LastLogonDate',
-    'lastLogon',
-    'lastLogonTimestamp',
-
-    'Modified',
-    'whenChanged',
-    'modifyTimeStamp',
-
-    'PasswordLastSet',
-    'pwdLastSet',
-
-    'SID'
-    'objectSid',
+    # LDAP attributes & PoSh properties differing only in case
+    'CanonicalName'                 # canonicalName
+    'CN'                            # cn
+    'DistinguishedName'             # distinguishedName
+    'ObjectGUID'                    # objectGUID
 
     # Interesting but duplicated properties
-    'mail'                  # EmailAddress
-    'mobile'                # MobilePhone
-    'sn'                    # Surname
+    'Deleted'                       # isDeleted
+)
+
+# AD class properties we may want to ignore: securityPrincipal
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignment', '')]
+$ADClassIgnoredPropertiesSecurityPrincipal = $ADClassIgnoredPropertiesTop + @(
+    # PoSh property followed by equivalent LDAP attribute
+    'SID', 'objectSid'
+
+    # LDAP attributes & PoSh properties differing only in case
+    'MemberOf'                      # memberOf
+)
+
+# AD class properties we may want to ignore: user
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignment', '')]
+$ADClassIgnoredPropertiesUser = $ADClassIgnoredPropertiesSecurityPrincipal + @(
+    # Only exposed via LDAP attributes
+    'lastLogoff'
+    'lastLogon'
+    'logonCount'
+    'msDS-KeyCredentialLink'
+
+    # PoSh property followed by equivalent LDAP attribute
+    'AccountLockoutTime', 'lockoutTime'
+    'BadLogonCount', 'badPwdCount'
+    'LastBadPasswordAttempt', 'badPasswordTime'
+    'LastLogonDate', 'lastLogonTimestamp'
+    'PasswordLastSet', 'pwdLastSet'
+
+    # Interesting but duplicated properties: person
+    'OfficePhone'                   # telephoneNumber
+    'Surname'                       # sn
+
+    # Interesting but duplicated properties: organizationalPerson
+    'City'                          # l
+    'Country'                       # c
+    'Fax'                           # facsimileTelephoneNumber
+    'Office'                        # physicalDeliveryOfficeName
+    'OtherName'                     # middleName
+    'POBox'                         # postOfficeBox
+    'State'                         # st
+
+    # Interesting but duplicated properties: user
+    'AccountExpirationDate'         # accountExpires
+    'EmailAddress'                  # mail
+    'MobilePhone'                   # mobile
+    'Organization'                  # o
 )
