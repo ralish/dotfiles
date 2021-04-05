5cd22f7ecc81587842aa4a53e05d7092eb899cb8
Samuel D. Leslie <sdl@nexiom.net>
2021-04-05 10:09:02 +1000

    Add apt-cfgs alias for finding modified configs

--- sh/apps/apt.sh
+++ sh/apps/apt.sh
@@ -2,6 +2,10 @@
 
 # APT configuration
 if command -v apt-get > /dev/null; then
+    # Show configs which have been modified from the defaults
+    # Via: https://serverfault.com/a/90401
+    alias apt-cfgs='dpkg-query -W -f="\${Conffiles}\n" "*" | awk "OFS=\" \"{print \$2,\$1}" | LANG=C md5sum -c 2> /dev/null | awk -F": " "\$2 !~ /OK\$/{print \$1}" | sort | less'
+
     # Super useful alias to determine manually installed packages
     # Via: https://askubuntu.com/questions/2389/generating-list-of-manually-installed-packages-and-querying-individual-packages
     alias apt-pkgs='comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n "s/^Package: //p" | sort -u)'
