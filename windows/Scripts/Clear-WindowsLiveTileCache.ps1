a8401ba70505ea5bbe21d414ff39dec900a454ae
Samuel D. Leslie <sdl@nexiom.net>
2022-02-20 09:55:30 +1100

    Add script to clear the Windows Live Tile cache

---
+++ windows/Scripts/Clear-WindowsLiveTileCache.ps1
@@ -0,0 +1,16 @@
+[CmdletBinding()]
+Param()
+
+if (![Environment]::OSVersion.Version -eq 10) {
+    throw 'Script is only valid for Windows 10.'
+}
+
+$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ImmersiveShell\StateStore'
+
+try {
+    $null = Get-ItemProperty -Path $RegPath -ErrorAction Stop
+} catch {
+    throw 'Failed to retrieve registry key for Windows Live Tile cache.'
+}
+
+Set-ItemProperty -Path $RegPath -Name ResetCache -Value 1
