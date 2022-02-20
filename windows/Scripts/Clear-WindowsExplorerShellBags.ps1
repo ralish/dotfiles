e1c316abd33149967c1fe8639c7055afd4720f4e
Samuel D. Leslie <sdl@nexiom.net>
2022-02-20 11:04:09 +1100

    Add script to clear Windows Explorer shell bags

---
+++ windows/Scripts/Clear-WindowsExplorerShellBags.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding(SupportsShouldProcess)]
+Param()
+
+$RegPaths = @(
+    'HKCU:\Software\Microsoft\Windows\Shell',
+    'HKCU:\Software\Microsoft\Windows\ShellNoRoam',
+    'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell'
+)
+
+if ([Environment]::Is64BitOperatingSystem) {
+    $RegPaths += 'HKCU:\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell'
+}
+
+foreach ($RegPath in $RegPaths) {
+    $RegPathBags = Join-Path -Path $RegPath -ChildPath 'Bags'
+    $RegPathBagMRU = Join-Path -Path $RegPath -ChildPath 'BagMRU'
+
+    foreach ($RegPathToRemove in @($RegPathBags, $RegPathBagMRU)) {
+        if ($PSCmdlet.ShouldProcess($RegPathToRemove, 'Remove')) {
+            try {
+                Remove-Item -Path $RegPathToRemove -Recurse -ErrorAction Stop
+            } catch {
+                switch -Regex ($PSItem.FullyQualifiedErrorId) {
+                    '^PathNotFound,' { }
+                    Default { throw }
+                }
+            }
+        }
+    }
+}
