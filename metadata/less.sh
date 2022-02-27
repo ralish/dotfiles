3442a62d12e68b0d3d924dc57c076a7ad045d955
Samuel D. Leslie <sdl@nexiom.net>
2022-02-27 13:00:58 +1100

    Skip running lesskey for less version >= 582

--- metadata/less.sh
+++ metadata/less.sh
@@ -5,6 +5,17 @@ script_dir="$(dirname "${BASH_SOURCE[0]}")"
 # shellcheck source=metadata/templates/common.sh
 source "$script_dir/templates/common.sh"
 
+if ! command -v less > /dev/null; then
+    exit "$DETECTION_NOT_AVAILABLE"
+fi
+
+# less can read lesskey source from version 582 (deprecating lesskey)
+# shellcheck disable=SC2312
+less_version=$(less -V | grep -Eo '^less \S+' | grep -Eo '[0-9]+')
+if (( less_version >= 582 )); then
+    exit "$DETECTION_SUCCESS"
+fi
+
 if ! command -v lesskey > /dev/null; then
     exit "$DETECTION_NOT_AVAILABLE"
 fi
