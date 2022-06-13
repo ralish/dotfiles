f75095ef97aee8b3509bb70ec6cd37d1e82bcf6c
Samuel D. Leslie <sdl@nexiom.net>
2022-06-13 17:46:57 +1000

    Hide PowerShell banner by default

--- sh/apps/pwsh.sh
+++ sh/apps/pwsh.sh
@@ -4,6 +4,9 @@
 if command -v pwsh > /dev/null; then
     # Opt-out of telemetry
     export POWERSHELL_TELEMETRY_OPTOUT=true
+
+    # Hide banner by default
+    alias pwsh='pwsh -NoLogo'
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
