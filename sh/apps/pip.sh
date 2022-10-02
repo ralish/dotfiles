9cafd628540f1400b3e3f62b069e53f33448485f
Samuel D. Leslie <sdl@nexiom.net>
2022-10-02 20:16:09 +1100

    Add ~/.local/bin to path for pip via *nix shells

--- sh/apps/pip.sh
+++ sh/apps/pip.sh
@@ -19,6 +19,11 @@ if command -v pip > /dev/null ||
     if command -v salt-call > /dev/null; then
         export PIP_DISABLE_PIP_VERSION_CHECK=true
     fi
+
+    # Add local bin directory to PATH (for --user)
+    build_path "$HOME/.local/bin" "$PATH"
+    # shellcheck disable=SC2154
+    export PATH="$build_path"
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
