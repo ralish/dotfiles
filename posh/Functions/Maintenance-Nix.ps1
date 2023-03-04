13d312d8f3e0634d0a44b780c4850d7ad1b46874
Samuel D. Leslie <sdl@nexiom.net>
2023-03-05 10:27:09 +1100

    Update-Homebrew: Add -WhatIf support

--- posh/Functions/Maintenance-Nix.ps1
+++ posh/Functions/Maintenance-Nix.ps1
@@ -11,7 +11,7 @@ if (!(Start-DotFilesSection @DotFilesSection)) {
 
 # Update Homebrew & installed apps
 Function Update-Homebrew {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess)]
     [OutputType([Void], [PSCustomObject])]
     Param(
         [ValidateRange(-1, [Int]::MaxValue)]
@@ -41,18 +41,28 @@ Function Update-Homebrew {
     [String[]]$UpdateArgs = 'update'
     [String[]]$UpgradeArgs = 'upgrade', '--greedy'
     [String[]]$CleanupArgs = 'cleanup', '-s'
+    if ($WhatIfPreference) {
+        $UpgradeArgs += '--dry-run'
+        $CleanupArgs += '--dry-run'
+    }
 
-    Write-Progress @WriteProgressParams -Status 'Updating Homebrew' -PercentComplete 1
-    Write-Verbose -Message ('Updating Homebrew: brew {0}' -f ($UpdateArgs -join ' '))
-    $Result.Update = & brew @UpdateArgs
+    if ($WhatIfPreference -or $PSCmdlet.ShouldProcess('Homebrew', 'Update')) {
+        Write-Progress @WriteProgressParams -Status 'Updating Homebrew' -PercentComplete 1
+        Write-Verbose -Message ('Updating Homebrew: brew {0}' -f ($UpdateArgs -join ' '))
+        $Result.Update = & brew @UpdateArgs
+    }
 
-    Write-Progress @WriteProgressParams -Status 'Updating casks & formulae' -PercentComplete 20
-    Write-Verbose -Message ('Updating casks & formulae: brew {0}' -f ($UpgradeArgs -join ' '))
-    $Result.Upgrade = & brew @UpgradeArgs
+    if ($WhatIfPreference -or $PSCmdlet.ShouldProcess('Homebrew casks & formulae', 'Update')) {
+        Write-Progress @WriteProgressParams -Status 'Updating casks & formulae' -PercentComplete 20
+        Write-Verbose -Message ('Updating casks & formulae: brew {0}' -f ($UpgradeArgs -join ' '))
+        $Result.Upgrade = & brew @UpgradeArgs
+    }
 
-    Write-Progress @WriteProgressParams -Status 'Cleaning-up obsolete files' -PercentComplete 80
-    Write-Verbose -Message ('Cleaning-up obsolete files: brew {0}' -f ($CleanupArgs -join ' '))
-    $Result.Cleanup = & brew @CleanupArgs
+    if ($WhatIfPreference -or $PSCmdlet.ShouldProcess('Homebrew obsolete files', 'Remove')) {
+        Write-Progress @WriteProgressParams -Status 'Cleaning-up obsolete files' -PercentComplete 80
+        Write-Verbose -Message ('Cleaning-up obsolete files: brew {0}' -f ($CleanupArgs -join ' '))
+        $Result.Cleanup = & brew @CleanupArgs
+    }
 
     Write-Progress @WriteProgressParams -Completed
 
