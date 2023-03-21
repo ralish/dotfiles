ed18e66527455aec45da9d23325499f4bef458b9
Samuel D. Leslie <sdl@nexiom.net>
2023-03-21 15:28:28 +1100

    Add .profile for bash

---
+++ sh/profile.sh
@@ -0,0 +1,24 @@
+# shellcheck shell=sh
+
+# (da)sh
+# If invoked as a login shell, reads and executes commands from ~/.profile.
+#
+# bash
+# If invoked as an interactive login shell, or as a non-interactive shell with
+# the --login option, looks for and executes the first found of the following:
+# - ~/.bash_profile
+# - ~/.bash_login
+# - ~/.profile
+#
+# zsh
+# Not used.
+
+# Bash specific
+if [ -n "$BASH_VERSION" ]; then
+    # Source .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+	. "$HOME/.bashrc"
+    fi
+fi
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
