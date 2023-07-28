76816d766382e0416ec8f29823dd9ad8833dacb3
Samuel D. Leslie <sdl@nexiom.net>
2023-07-28 17:53:49 +1000

    Add note advising against using CMD AutoRun setting

--- cmd/AddAutoRun.cmd
+++ cmd/AddAutoRun.cmd
@@ -1,5 +1,12 @@
 @ECHO OFF
 
+@REM Using the AutoRun setting is almost always more trouble than it's worth.
+@REM Doing anything non-trivial will incur a performance penalty on account of
+@REM the sheer number of things that spawn a CMD process, but the bigger issue
+@REM is the negative interactions it can have with other programs due to the
+@REM changes it explicitly or implicitly makes in the CMD environment. Catching
+@REM all these cases is effectively impossible, and they're often not obvious.
+
 IF [%DOTFILES%] == [] (
     ECHO The %%DOTFILES%% variable has not been set.
     EXIT /B
