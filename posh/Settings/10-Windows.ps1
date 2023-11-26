f0992d9762b1fb11b1b286c0c1480520f20544ff
Samuel D. Leslie <sdl@nexiom.net>
2023-11-26 16:07:54 +1100

    Move Console API for Windows.ps1 into C# file

--- posh/Settings/10-Windows.ps1
+++ posh/Settings/10-Windows.ps1
@@ -64,59 +64,9 @@ Function Repair-ConHostVT100Bug {
 
     Write-Verbose -Message (Get-DotFilesMessage -Message 'Applying fix for ConHost VT100 tab stop width bug ...')
 
-    $ConsoleAPI = @'
-[Flags]
-public enum ConsoleModeInputFlags
-{
-    ENABLE_PROCESSED_INPUT          = 0x001,
-    ENABLE_LINE_INPUT               = 0x002,
-    ENABLE_ECHO_INPUT               = 0x004,
-    ENABLE_WINDOW_INPUT             = 0x008,
-    ENABLE_MOUSE_INPUT              = 0x010,
-    ENABLE_INSERT_MODE              = 0x020,
-    ENABLE_QUICK_EDIT_MODE          = 0x040,
-    ENABLE_EXTENDED_FLAGS           = 0x080,
-    ENABLE_AUTO_POSITION            = 0x100,
-    ENABLE_VIRTUAL_TERMINAL_INPUT   = 0x200
-}
-
-[Flags]
-public enum ConsoleModeOutputFlags
-{
-    ENABLE_PROCESSED_OUTPUT             = 0x01,
-    ENABLE_WRAP_AT_EOL_OUTPUT           = 0x02,
-    ENABLE_VIRTUAL_TERMINAL_PROCESSING  = 0x04,
-    DISABLE_NEWLINE_AUTO_RETURN         = 0x08,
-    ENABLE_LVB_GRID_WORLDWIDE           = 0x10
-}
-
-[Flags]
-public enum StdHandleDevices
-{
-    STD_INPUT_HANDLE    = -10,
-    STD_OUTPUT_HANDLE   = -11,
-    STD_ERROR_HANDLE    = -12
-}
-
-[DllImport("kernel32.dll", SetLastError = true)]
-public static extern bool GetConsoleMode(
-    IntPtr hConsoleHandle,
-    out uint lpMode
-);
-
-[DllImport("kernel32.dll", SetLastError = true)]
-public static extern IntPtr GetStdHandle(
-    int nStdHandle
-);
-
-[DllImport("kernel32.dll", SetLastError = true)]
-public static extern bool SetConsoleMode(
-    IntPtr hConsoleHandle,
-    uint dwMode
-);
-'@
-
     if (!('DotFiles.Console' -as [Type])) {
+        $ConsoleCode = Join-Path -Path $PoshSettingsPath -ChildPath '10-Windows.cs'
+        $ConsoleAPI = Get-Content -LiteralPath $ConsoleCode -Raw
         Add-Type -Namespace 'DotFiles' -Name 'Console' -MemberDefinition $ConsoleAPI
     }
 
