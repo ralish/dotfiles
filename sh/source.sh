edc36c9b81977b6c3c55b761d2ab3ccb622c90b6
Samuel D. Leslie <sdl@nexiom.net>
2024-05-29 14:22:00 +1000

    Tweak sh build_path to not prepend existing paths

--- sh/source.sh
+++ sh/source.sh
@@ -6,34 +6,82 @@
 # DESC: Combines two path variables and removes any duplicates
 # ARGS: $1 (required): Path(s) to join with the second argument
 #       $2 (optional): Path(s) to join with the first argument
+#       $3 (optional): Set to any value to add the path(s) provided in the
+#                      first argument to the start of the constructed list of
+#                      paths, removing any duplicate paths which already were
+#                      present in the path string. If unset, the paths will be
+#                      added only if they did not already exist in the list of
+#                      paths in the second argument, preserving the existing
+#                      ordering of paths. If a path is present multiple times
+#                      in the second argument the first instance of the path
+#                      will be used, and subsequent instances are removed.
 # OUTS: $build_path: The constructed path
 # NOTE: Heavily inspired by: https://unix.stackexchange.com/a/40973
 build_path() {
-    if [ -z "${1-}" ] || [ $# -gt 2 ]; then
+    if [ -z "${1-}" ] || [ $# -gt 3 ]; then
         printf "Invalid arguments passed to build_path()!"
         exit 2
     fi
 
-    temp_path="$1:"
-    if [ -n "${2-}" ]; then
-        temp_path="$temp_path$2:"
+    first_path="$1"
+    second_path="${2-}"
+    prioritise_first="${3-}"
+
+    # Join the paths if two arguments were provided
+    temp_path="$first_path:"
+    if [ -n "$second_path" ]; then
+        # If we're not going to prioritise paths in the first argument we need
+        # to remove the paths which are already present in the second argument.
+        if [ -z "$prioritise_first" ]; then
+            new_path=''
+
+            while [ -n "$temp_path" ]; do
+                # Grab the next path element from the first path argument
+                path_entry="${temp_path%%:*}"
+
+                # Add the path if it's not present in the second path argument
+                case ":$second_path:" in
+                    *:"$path_entry":*) ;;
+                                    *) new_path="$new_path:$path_entry"
+                                       ;;
+                esac
+
+                # Remove processed path element
+                temp_path="${temp_path#*:}"
+            done
+
+            # Remove leading colon from and add trailing colon to the updated
+            # first argument paths.
+            temp_path="${new_path#:}:"
+        fi
+
+        temp_path="$temp_path$second_path:"
     fi
 
+    # Now loop over the path elements in the combined arguments (or just the
+    # first argument if no second argument was provided) and remove duplicates.
     new_path=''
     while [ -n "$temp_path" ]; do
+        # Grab the next path element
         path_entry="${temp_path%%:*}"
-        case "$new_path:" in
+
+        # Add the path if it's not present
+        case ":$new_path:" in
             *:"$path_entry":*) ;;
                             *) new_path="$new_path:$path_entry"
                                ;;
         esac
+
+        # Remove processed path element
         temp_path="${temp_path#*:}"
     done
 
+    # Remove leading colon from the final list of paths
     # shellcheck disable=SC2034
     build_path="${new_path#:}"
 
     unset new_path path_entry temp_path
+    unset first_path second_path prioritise_first
 }
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
