74c25e4a49d6598f9a2fc050401f1ac7b875c196
Samuel D. Leslie <sdl@nexiom.net>
2024-08-05 20:16:33 +1000

    Opt-out of Turborepo telemetry

---
+++ sh/apps/turborepo.sh
@@ -0,0 +1,9 @@
+# shellcheck shell=sh
+
+# Turborepo configuration
+if command -v turbo > /dev/null; then
+    # Opt-out of telemetry
+    export TURBO_TELEMETRY_DISABLED=1
+fi
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
