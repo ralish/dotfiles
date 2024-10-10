da6f2cf4834198b6e2f0e54c48378e5ae9462d3c
Samuel D. Leslie <sdl@nexiom.net>
2024-10-10 15:47:32 +1100

    Numerous improvements to app download scripts

--- scripts/lib/dl-shellcheck.sh
+++ scripts/lib/dl-shellcheck.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Downloads or updates shellcheck to the latest available version
-
+# Application name
+readonly APP_NAME="shellcheck"
 # Local installation path
-SHCHK_DST="$HOME/bin/shellcheck"
+readonly APP_DST="$HOME/bin/shellcheck"
 # Local installation directory
-SHCHK_DST_DIR="$HOME/bin"
+readonly APP_DST_DIR="$HOME/bin"
 # Latest release information
-SHCHK_LATEST_RELEASE='https://api.github.com/repos/koalaman/shellcheck/releases/latest'
+readonly APP_LATEST_RELEASE='https://api.github.com/repos/koalaman/shellcheck/releases/latest'
 
-# DESC: Update to latest shellcheck
+# DESC: Download latest ShellCheck
 # ARGS: None
 # OUTS: None
 # RETS: None
@@ -19,20 +19,20 @@ function dl_shellcheck() {
     check_binary xz fatal
 
     local current_ver
-    if [[ -x $SHCHK_DST ]]; then
-        current_ver="$("$SHCHK_DST" --version | grep -E '^version: .*' | grep -Eo '[0-9]+(\.[0-9]+){1,}')"
-        verbose_print "[shellcheck] Existing version: v$current_ver"
+    if [[ -x $APP_DST ]]; then
+        current_ver="$("$APP_DST" --version | grep -E '^version: .*' | grep -Eo '[0-9]+(\.[0-9]+){1,}')"
+        verbose_print "[$APP_NAME] Existing version: v$current_ver"
     else
-        verbose_print "[shellcheck] Found no existing binary."
+        verbose_print "[$APP_NAME] Found no existing binary."
     fi
 
-    verbose_print "[shellcheck] Retrieving release info ..."
+    verbose_print "[$APP_NAME] Retrieving release info ..."
     local metadata latest_ver
-    metadata="$(curl -s "$SHCHK_LATEST_RELEASE")"
+    metadata="$(curl -s "$APP_LATEST_RELEASE")"
     latest_ver="$(echo "$metadata" | jq -r '.tag_name' | grep -Eo '[0-9]+(\.[0-9]+){1,}')"
 
     if [[ $latest_ver == "${current_ver-}" ]]; then
-        pretty_print "[shellcheck] Latest version is installed: v$current_ver"
+        pretty_print "[$APP_NAME] Latest version is installed: v$current_ver"
         exit 0
     fi
 
@@ -40,34 +40,42 @@ function dl_shellcheck() {
     kernel_name_raw="$(uname -s)"
     kernel_name="${kernel_name_raw,,}"
     if ! [[ $kernel_name =~ darwin|linux ]]; then
-        script_exit "[shellcheck] Unsupported kernel: $kernel_name_raw" 1
+        script_exit "[$APP_NAME] Unsupported kernel: $kernel_name_raw" 1
     fi
 
-    local machine_hw_name
-    machine_hw_name="$(uname -m)"
-    if ! [[ $machine_hw_name =~ aarch64|x86_64 ]]; then
-        script_exit "[shellcheck] Unsupported machine hardware: $machine_hw_name" 1
-    fi
+    local machine_hw_name machine_hw_name_raw
+    machine_hw_name_raw="$(uname -m)"
+    case $machine_hw_name_raw in
+        armv6*)
+            machine_hw_name='armv6hf'
+            ;;
+        aarch64 | riscv64 | x86_64)
+            machine_hw_name="$machine_hw_name_raw"
+            ;;
+        *)
+            script_exit "[$APP_NAME] Unsupported machine hardware: $machine_hw_name_raw" 1
+            ;;
+    esac
 
     local latest_contains
     latest_contains="${kernel_name}.${machine_hw_name}"
-    verbose_print "[shellcheck] Filtering for release containing: $latest_contains"
+    verbose_print "[$APP_NAME] Filtering for release containing: $latest_contains"
 
     local latest_url
     latest_url="$(echo "$metadata" | jq -Mr ".assets[] | select(.name | contains(\"$latest_contains\")) | .browser_download_url")"
     if [[ -z $latest_url ]]; then
-        script_exit "[shellcheck] Unable to find release for kernel & machine hardware: $latest_contains" 1
+        script_exit "[$APP_NAME] Unable to find release for kernel & machine hardware: $latest_contains" 1
     fi
 
     if [[ -n ${current_ver-} ]]; then
-        pretty_print "[shellcheck] Updating v$current_ver to v$latest_ver ..."
+        pretty_print "[$APP_NAME] Updating v$current_ver to v$latest_ver ..."
     else
-        pretty_print "[shellcheck] Installing v$latest_ver ..."
+        pretty_print "[$APP_NAME] Installing v$latest_ver ..."
     fi
-    if ! [[ -d $SHCHK_DST_DIR ]]; then
-        mkdir "$SHCHK_DST_DIR"
+    if ! [[ -d $APP_DST_DIR ]]; then
+        mkdir "$APP_DST_DIR"
     fi
-    curl -sSL "$latest_url" | tar -x -J -C "$SHCHK_DST_DIR" --strip-components=1 --wildcards "*/shellcheck"
+    curl -sSL "$latest_url" | tar -x -J -C "$APP_DST_DIR" --strip-components=1 --wildcards "*/$APP_NAME"
 }
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
--- scripts/lib/dl-yq.sh
+++ scripts/lib/dl-yq.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Downloads or updates yq to the latest available version
-
+# Application name
+readonly APP_NAME="yq"
 # Local installation path
-YQ_DST="$HOME/bin/yq"
+readonly APP_DST="$HOME/bin/yq"
 # Local installation directory
-YQ_DST_DIR="$HOME/bin"
+readonly APP_DST_DIR="$HOME/bin"
 # Latest release information
-YQ_LATEST_RELEASE='https://api.github.com/repos/mikefarah/yq/releases/latest'
+readonly APP_LATEST_RELEASE='https://api.github.com/repos/mikefarah/yq/releases/latest'
 
-# DESC: Update to latest yq
+# DESC: Download latest yq
 # ARGS: None
 # OUTS: None
 # RETS: None
@@ -19,20 +19,20 @@ function dl_yq() {
     check_binary xz fatal
 
     local current_ver
-    if [[ -x $YQ_DST ]]; then
-        current_ver="$("$YQ_DST" --version | grep -Eo '[0-9]+(\.[0-9]+){1,}')"
-        verbose_print "[yq] Existing version: v$current_ver"
+    if [[ -x $APP_DST ]]; then
+        current_ver="$("$APP_DST" --version | grep -Eo '[0-9]+(\.[0-9]+){1,}')"
+        verbose_print "[$APP_NAME] Existing version: v$current_ver"
     else
-        verbose_print "[yq] Found no existing binary."
+        verbose_print "[$APP_NAME] Found no existing binary."
     fi
 
-    verbose_print "[yq] Retrieving release info ..."
+    verbose_print "[$APP_NAME] Retrieving release info ..."
     local metadata latest_ver
-    metadata="$(curl -s "$YQ_LATEST_RELEASE")"
+    metadata="$(curl -s "$APP_LATEST_RELEASE")"
     latest_ver="$(echo "$metadata" | jq -r '.tag_name' | grep -Eo '[0-9]+(\.[0-9]+){1,}')"
 
     if [[ $latest_ver == "${current_ver-}" ]]; then
-        pretty_print "[yq] Latest version is installed: v$current_ver"
+        pretty_print "[$APP_NAME] Latest version is installed: v$current_ver"
         exit 0
     fi
 
@@ -40,48 +40,54 @@ function dl_yq() {
     kernel_name_raw="$(uname -s)"
     kernel_name="${kernel_name_raw,,}"
     if ! [[ $kernel_name =~ darwin|freebsd|linux|netbsd|openbsd ]]; then
-        script_exit "[yq] Unsupported kernel: $kernel_name_raw" 1
+        script_exit "[$APP_NAME] Unsupported kernel: $kernel_name_raw" 1
     fi
 
     local machine_hw_name machine_hw_name_raw
     machine_hw_name_raw="$(uname -m)"
     case $machine_hw_name_raw in
-    aarch64)
-        machine_hw_name='arm64'
-        ;;
-    i386 | i686)
-        machine_hw_name='386'
-        ;;
-    x86_64)
-        machine_hw_name='amd64'
-        ;;
-    *)
-        script_exit "[yq] Unsupported machine hardware: $machine_hw_name_raw" 1
-        ;;
+        aarch64)
+            machine_hw_name='arm64'
+            ;;
+        armv6* | armv7*)
+            machine_hw_name='arm'
+            ;;
+        i686)
+            machine_hw_name='386'
+            ;;
+        x86_64)
+            machine_hw_name='amd64'
+            ;;
+        mips | mips64 | ppc64 | ppc64le | s390x)
+            machine_hw_name="$machine_hw_name_raw"
+            ;;
+        *)
+            script_exit "[$APP_NAME] Unsupported machine hardware: $machine_hw_name_raw" 1
+            ;;
     esac
 
     local latest_contains latest_contains_tgz
     latest_contains="${kernel_name}_${machine_hw_name}"
     latest_contains_tgz="$latest_contains.tar.gz"
-    verbose_print "[yq] Filtering for release containing: $latest_contains"
+    verbose_print "[$APP_NAME] Filtering for release containing: $latest_contains"
 
     local latest_url
     latest_url="$(echo "$metadata" | jq -Mr ".assets[] | select(.name | contains(\"$latest_contains_tgz\")) | .browser_download_url")"
     if [[ -z $latest_url ]]; then
-        script_exit "[yq] Unable to find release for kernel & machine hardware: $latest_contains" 1
+        script_exit "[$APP_NAME] Unable to find release for kernel & machine hardware: $latest_contains" 1
     fi
 
     if [[ -n ${current_ver-} ]]; then
-        pretty_print "[yq] Updating v$current_ver to v$latest_ver ..."
+        pretty_print "[$APP_NAME] Updating v$current_ver to v$latest_ver ..."
     else
-        pretty_print "[yq] Installing v$latest_ver ..."
+        pretty_print "[$APP_NAME] Installing v$latest_ver ..."
     fi
-    if ! [[ -d $YQ_DST_DIR ]]; then
-        mkdir "$YQ_DST_DIR"
+    if ! [[ -d $APP_DST_DIR ]]; then
+        mkdir "$APP_DST_DIR"
     fi
-    curl -sSL "$latest_url" | tar -x -z -C "$YQ_DST_DIR" --strip-components=1 --wildcards "*/yq_$latest_contains"
-    mv "$YQ_DST_DIR/yq_$latest_contains" "$YQ_DST"
-    chmod +x "$YQ_DST"
+    curl -sSL "$latest_url" | tar -x -z -C "$APP_DST_DIR" --strip-components=1 --wildcards "*/APP_$latest_contains"
+    mv "$APP_DST_DIR/APP_$latest_contains" "$APP_DST"
+    chmod +x "$APP_DST"
 }
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
