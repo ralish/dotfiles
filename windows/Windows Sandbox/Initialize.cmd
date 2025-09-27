9a6159667865f25f526846a93eb7675d3219e170
Samuel D. Leslie <sdl@nexiom.net>
2025-09-27 23:22:12 +1000

    Minor updates to Windows Sandbox configuration

--- windows/Windows Sandbox/Initialize.cmd
+++ windows/Windows Sandbox/Initialize.cmd
@@ -5,4 +5,4 @@ CD /D "%~dp0"
 
 @REM Add Sysinternals to system path
 FOR /F "usebackq tokens=2,*" %%A IN (`REG QUERY "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path`) DO SET PATH_MACHINE=%%B
-SETX /M PATH "C:\Program Files (x86)\Sysinternals;%PATH_MACHINE%"
+SETX /M PATH "%PATH_MACHINE%;%ProgramFiles(x86)%\Sysinternals"
