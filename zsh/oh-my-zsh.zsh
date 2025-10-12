56c14f305669da7bbfd4042abf0956acf41095ba
Samuel D. Leslie <sdl@nexiom.net>
2025-10-12 14:17:56 +1100

    Remove ripgrep plugin from Zsh profile

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -35,7 +35,7 @@ fi
 # --------------------------------- Plugins -----------------------------------
 
 # Array of plugins to load
-plugins=(colored-man-pages gitfast ripgrep shrink-path zsh-autosuggestions)
+plugins=(colored-man-pages gitfast shrink-path zsh-autosuggestions)
 
 # ------------------------------- Completion ----------------------------------
 
