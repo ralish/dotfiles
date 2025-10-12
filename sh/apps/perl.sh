8d1da40288652d70a81882b2fc48e3f50925c614
Samuel D. Leslie <sdl@nexiom.net>
2025-10-12 11:53:28 +1100

    Simplified creation of Perl environment on *nix

--- sh/apps/perl.sh
+++ sh/apps/perl.sh
@@ -2,25 +2,15 @@
 
 # Perl configuration
 if df_app_load 'Perl [perl]' 'command -v perl > /dev/null'; then
-    perl5_dir="${HOME}/perl5"
-
-    # Setup local Perl environment
-    if [ -d "$perl5_dir" ]; then
-        build_path "${perl5_dir}/bin" "$PATH"
-        # shellcheck disable=SC2154
-        export PATH="$build_path"
-
-        build_path "${perl5_dir}/lib/perl5" "$PERL5LIB"
-        export PERL5LIB="$build_path"
-
-        build_path "$perl5_dir" "$PERL_LOCAL_LIB_ROOT"
-        export PERL_LOCAL_LIB_ROOT="$build_path"
-
-        export PERL_MB_OPT="--install_base \"${perl5_dir}\""
-        export PERL_MM_OPT="INSTALL_BASE=${perl5_dir}"
+    # Check if local::lib is available
+    if ! perl -e 'eval "require local::lib"; if ($@) { exit 1 }'; then
+        echo '[dotfiles] Perl found but local:lib module not available.'
+        return
     fi
 
-    unset perl5_dir
+    # Setup the local Perl environment
+    # shellcheck disable=SC2312
+    eval "$(perl -Mlocal::lib)"
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
