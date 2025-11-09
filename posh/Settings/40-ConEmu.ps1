5ee333a3fc623f12ddf6a612ca33e4ee9448f71f
Samuel D. Leslie <sdl@nexiom.net>
2025-11-09 17:43:38 +1100

    Documentation improvements for ConEmu PowerShell integration

--- posh/Settings/40-ConEmu.ps1
+++ posh/Settings/40-ConEmu.ps1
@@ -10,17 +10,19 @@ if (!(Start-DotFilesSection @DotFilesSection)) {
     return
 }
 
-# Special prompt handling for PowerShell under ConEmu
-# See: https://conemu.github.io/en/PowershellPrompt.html
+# Extending PowerShell Prompt
+# https://conemu.github.io/en/PowershellPrompt.html
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
 $ConEmuPrompt = {
-    # Let ConEmu know where the prompt ends
-    $Prompt = '{0}]9;12{1}' -f [Char]27, [Char]7
+    # Mark end of prompt
+    # OSC ConEmu ; PromptStart ST
+    $Prompt = "{0}]9;12`a" -f [Char]27
 
-    # Let ConEmu know the current working dir
+    # Mark current working directory
     $CurLoc = $ExecutionContext.SessionState.Path.CurrentLocation
     if ($CurLoc.Provider.Name -eq 'FileSystem') {
-        $Prompt += '{0}]9;9;"{1}"{2}' -f [Char]27, $CurLoc, [Char]7
+        # OSC ConEmu ; CurrentDir ; "<Cwd>" ST
+        $Prompt += "{0}]9;9;`"{1}`"`a" -f [Char]27, $CurLoc
     }
 
     return $Prompt
