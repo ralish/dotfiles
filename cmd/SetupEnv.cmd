e732cd53295e5c8198349cf15346c9522dce13a5
Samuel D. Leslie <sdl@nexiom.net>
2025-11-09 17:43:00 +1100

    Major overhaul of SetupEnv.cmd script

--- cmd/SetupEnv.cmd
+++ cmd/SetupEnv.cmd
@@ -6,127 +6,195 @@
 
 @REM Uncomment to enable verbose mode
 @REM SET SetupEnvVerbose=Yes
-IF DEFINED SetupEnvVerbose ECHO.
-IF DEFINED SetupEnvVerbose ECHO Configuring Command Processor:
-
-@REM Switch to the user's profile directory if the current path is the Windows
-@REM System32 directory. This likely means we launched as an elevated process.
-IF /I "%CD%" == "%SystemRoot%\System32" (
-    CD /D "%USERPROFILE%"
+IF DEFINED SetupEnvVerbose (
+    ECHO.
+    ECHO Configuring Command Processor:
 )
 
-@REM ##########################################################################
-@REM ###                             Clink                                  ###
-@REM ##########################################################################
-
-WHERE /Q "clink.cmd"
-IF NOT ERRORLEVEL 1 (
-    @REM Found in %%PATH%% (likely installed via Scoop or similar)
-    SET ClinkPath="clink.cmd"
-    GOTO ClinkRun
-)
-IF EXIST "%CLINK_DIR%" (
-    @REM Found via %%CLINK_DIR%% (set via Clink's own installer)
-    SET ClinkPath="%CLINK_DIR%\clink.bat"
-    GOTO ClinkRun
-)
-GOTO ClinkSkip
-
-:ClinkRun
-IF DEFINED SetupEnvVerbose ECHO * [Clink] Injecting ...
-CALL "%ClinkPath%" inject --autorun --profile ~\clink
-SET ClinkPath=
-GOTO ClinkEnd
-
-:ClinkSkip
-IF DEFINED SetupEnvVerbose ECHO * [Clink] Not found.
-
-:ClinkEnd
+@REM If Command Processor is launched elevated the current working directory
+@REM will initially be the Windows `System32` directory. In that case, change
+@REM to our user profile directory, which is where we most likely want to be.
+IF /I "%CD%" == "%SystemRoot%\System32" CD /D "%USERPROFILE%"
 
 @REM ##########################################################################
-@REM ###                            ANSICON                                 ###
-@REM ##########################################################################
-
-@REM ConEmu provides its own ANSI support
-IF DEFINED ConEmuANSI GOTO AnsiConEnd
-
-WHERE /Q "ansicon.exe"
-IF ERRORLEVEL 1 (
-    GOTO AnsiConSkip
-)
-
-IF DEFINED SetupEnvVerbose ECHO * [ANSICON] Injecting ...
-IF !ANSICON_VER! == ^!ANSICON_VER^! "ansicon.exe" -p
-GOTO AnsiConEnd
-
-:AnsiConSkip
-IF DEFINED SetupEnvVerbose ECHO * [ANSICON] Not found.
-
-:AnsiConEnd
-
-@REM ##########################################################################
-@REM ###                            ConEmu                                  ###
-@REM ##########################################################################
-
-@REM Check we're running under ConEmu
-IF NOT DEFINED ConEmuDir GOTO ConEmuEnd
-
-@REM Setup a more informative prompt
-IF DEFINED SetupEnvVerbose ECHO * [ConEmu] Configuring prompt ...
-IF "%ConEmuIsAdmin%" == "ADMIN" (
-    PROMPT $E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[91m$P$E[90m$G$E[m$S$E]9;12$E\
-) ELSE (
-    PROMPT $E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m$G$E[m$S$E]9;12$E\
-)
-
-:ConEmuEnd
-
-@REM ##########################################################################
-@REM ###                        Windows Terminal                            ###
+@REM ###                       Windows Terminal                             ###
 @REM ##########################################################################
 
 @REM Shell Integration: Command Prompt
 @REM https://learn.microsoft.com/en-au/windows/terminal/tutorials/shell-integration#command-prompt
 
-@REM Check we're running under Windows Terminal
-IF NOT DEFINED WT_SESSION GOTO WinTermEnd
+:WinTermDetect
+IF NOT DEFINED WT_SESSION GOTO WinTermSkip
 
-@REM Setup a more informative prompt
-IF DEFINED SetupEnvVerbose ECHO * [Windows Terminal] Configuring prompt ...
+:WinTermFound
+SET HasWinTerm=Yes
+IF DEFINED SetupEnvVerbose ECHO * [Windows Terminal] Detected.
+GOTO :ClinkDetect
+
+:WinTermSkip
+SET HasWinTerm=
+IF DEFINED SetupEnvVerbose ECHO * [Windows Terminal] Not found.
+
+@REM ##########################################################################
+@REM ###                            ConEmu                                  ###
+@REM ##########################################################################
+
+@REM Configuring Cmd Prompt
+@REM https://conemu.github.io/en/CmdPrompt.html
+
+:ConEmuDetect
+IF NOT DEFINED ConEmuDir GOTO ConEmuSkip
+
+:ConEmuFound
+SET HasConEmu=Yes
+IF DEFINED SetupEnvVerbose ECHO * [ConEmu] Detected.
+GOTO :ClinkDetect
+
+:ConEmuSkip
+SET HasConEmu=
+IF DEFINED SetupEnvVerbose ECHO * [ConEmu] Not found.
+
+@REM ##########################################################################
+@REM ###                            ANSICON                                 ###
+@REM ##########################################################################
+
+:AnsiConDetect
+WHERE /Q "ansicon.exe"
+IF ERRORLEVEL 1 GOTO AnsiConSkip
+
+:AnsiConFound
+SET HasAnsiCon=Yes
+IF DEFINED SetupEnvVerbose ECHO * [ANSICON] Injecting ...
+IF !ANSICON_VER! == ^!ANSICON_VER^! "ansicon.exe" -p
+GOTO ClinkDetect
+
+:AnsiConSkip
+SET HasAnsiCon=
+IF DEFINED SetupEnvVerbose ECHO * [ANSICON] Not found.
+
+@REM ##########################################################################
+@REM ###                             Clink                                  ###
+@REM ##########################################################################
+
+:ClinkDetect
+WHERE /Q "clink.cmd"
+IF NOT ERRORLEVEL 1 (
+    @REM Found in %%PATH%% (likely installed via Scoop or similar)
+    SET ClinkPath="clink.cmd"
+    GOTO ClinkFound
+)
+IF EXIST "%CLINK_DIR%" (
+    @REM Found via %%CLINK_DIR%% (set via Clink's own installer)
+    SET ClinkPath="%CLINK_DIR%\clink.bat"
+    GOTO ClinkFound
+)
+GOTO ClinkSkip
+
+:ClinkFound
+SET HasClink=Yes
+IF DEFINED SetupEnvVerbose ECHO * [Clink] Injecting ...
+CALL "%ClinkPath%" inject --autorun --profile ~\clink
+SET ClinkPath=
+GOTO PromptSetup
+
+:ClinkSkip
+SET HasClink=
+IF DEFINED SetupEnvVerbose ECHO * [Clink] Not found.
+
+@REM ##########################################################################
+@REM ###                     Prompt customisation                           ###
+@REM ##########################################################################
+
+:PromptSetup
+SET CustomPrompt=
+IF DEFINED SetupEnvVerbose ECHO * [Prompt] Configuring ...
+IF DEFINED HasWinTerm GOTO :PromptStartWinTerm
+IF DEFINED HasConEmu GOTO :PromptAnsi
+IF DEFINED HasAnsiCon GOTO :PromptAnsi
+IF DEFINED HasClink GOTO :PromptAnsi
+
+:PromptBasic
+@REM Setup basic prompt
+@REM $P (Current drive and path)
+@REM $G$S ("> ")
+SET CustomPrompt=$P$G$S
+GOTO PromptApply
+
+:PromptStartWinTerm
+@REM Mark end of last command and start of prompt
+@REM Applies to: Windows Terminal
+@REM
+@REM The OSC code to pass the current working directory to the terminal was
+@REM initially introduced by ConEmu and is also supported by Windows Terminal.
+@REM We only apply it on Windows Terminal as modern ConEmu releases provide
+@REM this support through the ConEmuHk library which is injected into CMD.
+@REM
 @REM OSC FinalTerm ; CmdEnd ST
 @REM OSC FinalTerm ; PromptStart ST
-@REM OSC ConEmu ; CurrentDir ; <Cwd> ST
-@REM <Prompt>
-@REM OSC FinalTerm ; CmdStart ST
-PROMPT $e]133;D$e\$e]133;A$e\$e]9;9;$P$e\$P$G$e]133;B$e\
+@REM OSC ConEmu ; CurrentDir ; "<Cwd>" ST
+SET CustomPrompt=$E]133;D$E\$E]133;A$E\$E]9;9;$P$E\
 
-:WinTermEnd
+:PromptAnsi
+@REM Setup coloured prompt
+@REM Applies to: ANSICON, Clink, ConEmu, and Windows Terminal
+@REM
+@REM SGR ResetAttributes
+@REM SGR SetBrightForegroundColour 2
+@REM $P (Current drive and path)
+@REM SGR SetBrightForegroundColour 0
+@REM $G (">")
+@REM SGR ResetAttributes
+@REM $S (" ")
+SET CustomPrompt=%CustomPrompt%$E[m$E[92m$P$E[90m$G$E[m$S
+IF DEFINED HasWinTerm GOTO PromptEndWinTerm
+IF DEFINED HasConEmu GOTO PromptEndConEmu
+GOTO PromptApply
+
+:PromptEndWinTerm
+@REM Mark end of prompt
+@REM Applies to: Windows Terminal
+@REM
+@REM OSC FinalTerm ; CmdStart ST
+SET CustomPrompt=%CustomPrompt%$E]133;B$E\
+GOTO PromptApply
+
+:PromptEndConEmu
+@REM Mark start of input
+@REM Applies to: ConEmu
+@REM
+@REM OSC ConEmu ; PromptStart ST
+SET CustomPrompt=%CustomPrompt%$E]9;12$E\
+
+:PromptApply
+PROMPT %CustomPrompt%
+SET CustomPrompt=
 
 @REM ##########################################################################
 @REM ###                            DOSKEY                                  ###
 @REM ##########################################################################
 
+:DosKeyDetect
 WHERE /Q "doskey.exe"
-IF ERRORLEVEL 1 (
-    GOTO DosKeySkip
-)
+IF ERRORLEVEL 1 GOTO DosKeySkip
 
-@REM Because I'm tired of forgetting this is not a *nix shell
+:DosKeySetup
 IF DEFINED SetupEnvVerbose ECHO * [DOSKEY] Configuring ...
 DOSKEY clear=cls $*
 DOSKEY ls=dir $*
 DOSKEY man=help $*
 DOSKEY which=where $*
-GOTO DosKeyEnd
+GOTO Cleanup
 
 :DosKeySkip
 IF DEFINED SetupEnvVerbose ECHO * [DOSKEY] Not found.
 
-:DosKeyEnd
-
 @REM ##########################################################################
-@REM ###                              End                                   ###
+@REM ###                           Clean-up                                 ###
 @REM ##########################################################################
 
-@REM Remove script variables
+:Cleanup
+SET HasAnsiCon=
+SET HasClink=
+SET HasConEmu=
+SET HasWinTerm=
 SET SetupEnvVerbose=
