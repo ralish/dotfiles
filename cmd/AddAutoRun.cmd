5e7b7bbbb698e8b12969102a447669296dcf4bd4
Samuel D. Leslie <sdl@nexiom.net>
2025-11-09 17:42:44 +1100

    Minor fix & tweaks to AddAutoRun.cmd script

--- cmd/AddAutoRun.cmd
+++ cmd/AddAutoRun.cmd
@@ -1,19 +1,20 @@
 @ECHO OFF
 
-@REM Using the AutoRun setting is almost always more trouble than it's worth.
+@REM Using the `AutoRun` setting is almost always more trouble than it's worth.
 @REM Doing anything non-trivial will incur a performance penalty on account of
-@REM the sheer number of things that spawn a CMD process, but the bigger issue
+@REM the large number of things that spawn a CMD process, but the bigger issue
 @REM is the negative interactions it can have with other programs due to the
-@REM changes it explicitly or implicitly makes in the CMD environment. Catching
+@REM changes it explicitly or implicitly makes to the CMD environment. Catching
 @REM all these cases is effectively impossible, and they're often not obvious.
 
-IF [%DOTFILES%] == [] (
+IF NOT DEFINED DOTFILES (
     ECHO The %%DOTFILES%% variable has not been set.
     EXIT /B
 )
 
+ECHO Setting HKCU\Software\Microsoft\Command Processor\AutoRun ...
 REG ADD "HKCU\Software\Microsoft\Command Processor" ^
-    /v AutoRun ^
+    /v "AutoRun" ^
     /t REG_EXPAND_SZ ^
     /d """%%DOTFILES%%\cmd\SetupEnv.cmd""" ^
     /f
