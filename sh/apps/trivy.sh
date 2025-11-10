30a81a858bee9addfd18eb3b1efd50b963bd45db
Samuel D. Leslie <sdl@nexiom.net>
2025-11-10 16:49:56 +1100

    Enable shell completions for Trivy

--- sh/apps/trivy.sh
+++ sh/apps/trivy.sh
@@ -4,6 +4,16 @@
 if df_app_load 'trivy' 'command -v trivy > /dev/null'; then
     # Opt-out of telemetry
     export TRIVY_DISABLE_TELEMETRY=1
+
+    # Enable shell completion
+    # https://trivy.dev/docs/latest/tutorials/shell/shell-completion/
+    if [ -n "$BASH" ]; then
+        # shellcheck disable=SC2312,SC3001,SC3046,SC3051 source=/dev/null
+        source <(trivy completion bash)
+    elif [ -n "$ZSH_NAME" ]; then
+        # shellcheck disable=SC2312,SC3001,SC3046,SC3051 source=/dev/null
+        source <(trivy completion zsh)
+    fi
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
