58826ac60150af32decec23abd781e22e8c4b056
Samuel D. Leslie <sdl@nexiom.net>
2025-11-16 10:55:55 +1100

    Add shell completion support for Gitleaks, OPA, Regal, Turborep, and yq

---
+++ sh/apps/regal.sh
@@ -0,0 +1,15 @@
+# shellcheck shell=sh
+
+# Regal configuration
+if df_app_load 'regal' 'command -v regal > /dev/null'; then
+    # Enable shell completion
+    if [ -n "$BASH" ]; then
+        # shellcheck disable=SC2312,SC3001,SC3046,SC3051 source=/dev/null
+        source <(regal completion bash)
+    elif [ -n "$ZSH_NAME" ]; then
+        # shellcheck disable=SC2312,SC3001,SC3046,SC3051 source=/dev/null
+        source <(regal completion zsh)
+    fi
+fi
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
--- sh/apps/turborepo.sh
+++ sh/apps/turborepo.sh
@@ -3,7 +3,20 @@
 # Turborepo configuration
 if df_app_load 'turbo' 'command -v turbo > /dev/null'; then
     # Opt-out of telemetry
+    # https://turborepo.com/docs/telemetry
     export TURBO_TELEMETRY_DISABLED=1
+
+    # Enable shell completion
+    #
+    # Redirection of `stderr` is required to suppress the output of the
+    # Turborepo version number.
+    if [ -n "$BASH" ]; then
+        # shellcheck disable=SC2312,SC3001,SC3046,SC3051 source=/dev/null
+        source <(turbo completion bash 2> /dev/null)
+    elif [ -n "$ZSH_NAME" ]; then
+        # shellcheck disable=SC2312,SC3001,SC3046,SC3051 source=/dev/null
+        source <(turbo completion zsh 2> /dev/null)
+    fi
 fi
 
 # vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
