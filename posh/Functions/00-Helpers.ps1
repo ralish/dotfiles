fa8af5300d3292801ad3448a18fc11c0739a6334
Samuel D. Leslie <sdl@nexiom.net>
2026-04-25 12:18:24 +1000

    Minor tweaks to PowerShell helper functions

--- posh/Functions/00-Helpers.ps1
+++ posh/Functions/00-Helpers.ps1
@@ -1,11 +1,165 @@
+#region Environment tests
+
+# Confirm a command is available
+Function Test-CommandAvailable {
+    [CmdletBinding()]
+    [OutputType([Void])]
+    Param(
+        [Parameter(Mandatory)]
+        [String[]]$Name
+    )
+
+    foreach ($Command in $Name) {
+        Write-Debug -Message ('Checking command is available: {0}' -f $Command)
+
+        if (!(Get-Command -Name $Command -ErrorAction 'Ignore')) {
+            throw 'Required command not available: {0}' -f $Command
+        }
+    }
+}
+
+# Check environment matches expectations
+Function Test-EnvironmentMatch {
+    [CmdletBinding()]
+    [OutputType([Void])]
+    Param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [Hashtable]$Environment
+    )
+
+    foreach ($EnvName in $Environment.Keys) {
+        Write-Debug -Message ('Checking for environment variable: {0}' -f $EnvName)
+
+        $EnvExpectedValue = $Environment[$EnvName]
+        $EnvCurrentValue = [Environment]::GetEnvironmentVariable($EnvName)
+
+        if (!($EnvExpectedValue -is [Boolean] -or $EnvExpectedValue -is [String])) {
+            throw 'Value for key "{0}" is not a Boolean or String type.' -f $EnvName
+        }
+
+        # An empty string is an invalid environment variable value on Windows
+        if ((Test-IsWindows) -and $EnvExpectedValue -is [String] -and $EnvExpectedValue -eq [String]::Empty) {
+            throw 'Environment variable "{0}" cannot be set to an empty string on Windows.' -f $EnvName
+        }
+
+        if ($null -eq $EnvCurrentValue) {
+            throw 'Environment variable "{0}" is not set but expected "{1}".' -f $EnvName, $EnvExpectedValue
+        }
+
+        if ($EnvExpectedValue -is [Boolean]) {
+            # Environment variable must not exist
+            if ($EnvExpectedValue -eq $false) {
+                if ($null -eq $EnvCurrentValue) { continue }
+                throw 'Environment variable exists: {0}' -f $EnvName
+            }
+
+            # Environment variable must exist (any value)
+            if ($null -ne $EnvCurrentValue) { continue }
+            throw 'Environment variable not set: {0}' -f $EnvName
+        }
+
+        if ($EnvExpectedValue -ne $EnvCurrentValue) {
+            throw 'Environment variable "{0}" set to "{1}" but expected "{2}".' -f $EnvName, $EnvCurrentValue, $EnvExpectedValue
+        }
+    }
+}
+
+# Naive check for running on Windows
+Function Test-IsWindows {
+    [CmdletBinding()]
+    [OutputType([Boolean])]
+    Param()
+
+    if ($PSVersionTable.PSEdition -eq 'Desktop' -or $PSVersionTable.Platform -eq 'Win32NT') {
+        return $true
+    }
+
+    return $false
+}
+
+# Confirm a PowerShell module is available
+Function Test-ModuleAvailable {
+    [CmdletBinding()]
+    [OutputType([Void], [PSModuleInfo[]])]
+    Param(
+        [Parameter(Mandatory)]
+        [String[]]$Name,
+
+        [ValidateSet('Get', 'Import')]
+        [String]$Operation = 'Get',
+
+        [ValidateSet('Any', 'All')]
+        [String]$Require = 'All',
+
+        [Switch]$PassThru
+    )
+
+    if ($PassThru) {
+        $ModuleInfo = [Collections.Generic.List[PSModuleInfo]]::new()
+    }
+
+    foreach ($Module in $Name) {
+        Write-Debug -Message ('Checking module is available: {0}' -f $Module)
+
+        if ($Operation -eq 'Get') {
+            $ModuleAvailable = @(Get-Module -Name $Module -ListAvailable -Verbose:$false)
+        } else {
+            # Suppress verbose output on import
+            $VerboseOriginal = $Global:VerbosePreference
+            $Global:VerbosePreference = 'SilentlyContinue'
+
+            try {
+                Import-Module -Name $Module -ErrorAction 'Stop' -Verbose:$false
+            } finally {
+                # Restore the original `$VerbosePreference` setting
+                $Global:VerbosePreference = $VerboseOriginal
+            }
+
+            $ModuleAvailable = @(Get-Module -Name $Module -Verbose:$false)
+        }
+
+        if ($ModuleAvailable) {
+            $MissingModule = $false
+
+            if ($PassThru) {
+                $ModuleInfo.Add(($ModuleAvailable | Sort-Object -Property 'Version' -Descending | Select-Object -First 1))
+            }
+
+            if ($Require -eq 'Any') { break }
+        } else {
+            $MissingModule = $true
+            $MissingModuleName = $Module
+
+            if ($Require -eq 'All') { break }
+        }
+    }
+
+    if ($MissingModule) {
+        if ($Require -eq 'Any') {
+            throw 'Suitable module not available: {0}' -f ($Name -join ', ')
+        }
+
+        throw 'Required module not available: {0}' -f $MissingModuleName
+    }
+
+    if ($PassThru) {
+        return $ModuleInfo.ToArray()
+    }
+}
+
+#endregion
+
 #region Profile utilities
 
-# Complete a `dotfiles` section and potentially output timings
+# Complete a `dotfiles` section and conditionally output timings
 Function Complete-DotFilesSection {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '')]
     [CmdletBinding()]
     [OutputType([Void])]
     Param()
+
+    Write-Debug -Message (Get-DotFilesMessage -Message 'Completing section processing ...')
 
     if ($DotFilesShowTimings) {
         if ($Global:DotFilesSectionStart -isnot [DateTime]) {
@@ -16,7 +170,7 @@ Function Complete-DotFilesSection {
         Write-Verbose -Message (Get-DotFilesMessage -Message $Timing)
     }
 
-    Remove-Variable -Name 'DotFilesSection*' -Scope Global
+    Remove-Variable -Name 'DotFilesSection*' -Scope 'Global'
 }
 
 # Retrieve a formatted `dotfiles` message
@@ -82,10 +236,11 @@ Function Remove-DotFilesHelpers {
     Param()
 
     $Helpers = @(
-        '*-DotFilesSection'
+        'Complete-DotFilesSection'
         'Get-DotFilesMessage'
         'Get-DotFilesTiming'
         'Remove-DotFilesHelpers'
+        'Start-DotFilesSection'
     )
 
     foreach ($Helper in $Helpers) {
@@ -126,10 +281,13 @@ Function Start-DotFilesSection {
         # Test-ModuleAvailable
         [ValidateNotNullOrEmpty()]
         [String[]]$Module,
+
         [ValidateSet('Get', 'Import')]
         [String]$ModuleOperation = 'Get',
+
         [ValidateSet('Any', 'All')]
         [String]$ModuleRequire = 'All',
+
         [Switch]$ForceTestModule
     )
 
@@ -140,12 +298,18 @@ Function Start-DotFilesSection {
         $Global:DotFilesSectionStart = Get-Date
     }
 
-    if ($Platform -eq 'Windows' -and !(Test-IsWindows)) {
-        Write-Verbose -Message (Get-DotFilesMessage -Message 'Skipping as platform is not Windows.')
-        return $false
-    } elseif ($Platform -eq 'Unix' -and (Test-IsWindows)) {
-        Write-Verbose -Message (Get-DotFilesMessage -Message 'Skipping as platform is not Unix-like.')
-        return $false
+    Write-Debug -Message (Get-DotFilesMessage -Message 'Starting section processing ...')
+
+    if ($Platform) {
+        if ($Platform -eq 'Windows' -and !(Test-IsWindows)) {
+            Write-Verbose -Message (Get-DotFilesMessage -Message 'Skipping as platform is not Windows.')
+            return $false
+        }
+
+        if ($Platform -eq 'Unix' -and (Test-IsWindows)) {
+            Write-Verbose -Message (Get-DotFilesMessage -Message 'Skipping as platform is not Unix-like.')
+            return $false
+        }
     }
 
     if ($PwshMinVersion -and $PwshMinVersion -gt $PSVersionTable.PSVersion) {
@@ -191,163 +355,6 @@ Function Start-DotFilesSection {
 
     Write-Verbose -Message (Get-DotFilesMessage -Message 'All prerequisites met.')
     if ($Platform -or $Command -or $Module) { return $true }
-}
-
-#endregion
-
-#region Environment & platform tests
-
-# Confirm a PowerShell command is available
-Function Test-CommandAvailable {
-    [CmdletBinding()]
-    [OutputType([Void])]
-    Param(
-        [Parameter(Mandatory)]
-        [String[]]$Name
-    )
-
-    foreach ($Command in $Name) {
-        Write-Debug -Message ('Checking command is available: {0}' -f $Command)
-        if (!(Get-Command -Name $Command -ErrorAction Ignore)) {
-            throw 'Required command not available: {0}' -f $Command
-        }
-    }
-}
-
-# Check environment matches expectations
-Function Test-EnvironmentMatch {
-    [CmdletBinding()]
-    [OutputType([Void])]
-    Param(
-        [Parameter(Mandatory)]
-        [Hashtable]$Environment
-    )
-
-    foreach ($EnvName in $Environment.Keys) {
-        $EnvExpectedValue = $Environment[$EnvName]
-
-        if (!($EnvExpectedValue -is [Boolean] -or $EnvExpectedValue -is [String])) {
-            throw 'Value for key "{0}" is not a Boolean or String type.' -f $EnvName
-        }
-
-        $EnvCurrentValue = [Environment]::GetEnvironmentVariable($EnvName)
-
-        # An empty string is an invalid environment variable value on Windows
-        if ((Test-IsWindows) -and $EnvExpectedValue -is [String] -and $EnvExpectedValue -eq [String]::Empty) {
-            throw 'Environment variable "{0}" cannot be set to an empty string on Windows.' -f $EnvName
-        }
-
-        if ($EnvExpectedValue -is [Boolean]) {
-            # Environment variable must not exist
-            if ($EnvExpectedValue -eq $false) {
-                if ($null -ne $EnvCurrentValue) {
-                    throw 'Environment variable exists: {0}' -f $EnvName
-                }
-
-                continue
-            }
-
-            # Environment variable must exist (any value)
-            if ($null -eq $EnvCurrentValue) {
-                throw 'Environment variable not set: {0}' -f $EnvName
-            }
-
-            continue
-        }
-
-        if ($null -eq $EnvCurrentValue) {
-            throw 'Environment variable "{0}" is not set but expected "{1}".' -f $EnvName, $EnvExpectedValue
-        }
-
-        if ($EnvExpectedValue -ne $EnvCurrentValue) {
-            throw 'Environment variable "{0}" set to "{1}" but expected "{2}".' -f $EnvName, $EnvCurrentValue, $EnvExpectedValue
-        }
-    }
-}
-
-# Naive check for if we're running on Windows
-Function Test-IsWindows {
-    [CmdletBinding()]
-    [OutputType([Boolean])]
-    Param()
-
-    if ($PSVersionTable.PSEdition -eq 'Desktop' -or $PSVersionTable.Platform -eq 'Win32NT') {
-        return $true
-    }
-
-    return $false
-}
-
-# Confirm a PowerShell module is available
-Function Test-ModuleAvailable {
-    [CmdletBinding()]
-    [OutputType([Void], [PSModuleInfo[]])]
-    Param(
-        [Parameter(Mandatory)]
-        [String[]]$Name,
-
-        [ValidateSet('Get', 'Import')]
-        [String]$Operation = 'Get',
-
-        [ValidateSet('Any', 'All')]
-        [String]$Require = 'All',
-
-        [Switch]$PassThru
-    )
-
-    if ($PassThru) {
-        $ModuleInfo = [Collections.Generic.List[PSModuleInfo]]::new()
-    }
-
-    foreach ($Module in $Name) {
-        Write-Debug -Message ('Checking module is available: {0}' -f $Module)
-
-        if ($Operation -eq 'Get') {
-            $ModuleAvailable = @(Get-Module -Name $Module -ListAvailable -Verbose:$false)
-        } else {
-            # Suppress verbose output on import
-            $VerboseOriginal = $Global:VerbosePreference
-            $Global:VerbosePreference = 'SilentlyContinue'
-
-            try {
-                Import-Module -Name $Module -ErrorAction Stop -Verbose:$false
-            } catch {
-                throw $_
-            } finally {
-                # Restore the original `$VerbosePreference` setting
-                $Global:VerbosePreference = $VerboseOriginal
-            }
-
-            $ModuleAvailable = @(Get-Module -Name $Module -Verbose:$false)
-        }
-
-        if ($ModuleAvailable) {
-            $MissingModule = $false
-
-            if ($PassThru) {
-                $ModuleInfo.Add(($ModuleAvailable | Sort-Object -Property 'Version' -Descending | Select-Object -First 1))
-            }
-
-            if ($Require -eq 'Any') { break }
-        } else {
-            $MissingModule = $true
-            $MissingModuleName = $Module
-
-            if ($Require -eq 'All') { break }
-        }
-    }
-
-    if ($MissingModule) {
-        if ($Require -eq 'Any') {
-            throw 'Suitable module not available: {0}' -f ($Name -join ', ')
-        }
-
-        throw 'Required module not available: {0}' -f $MissingModuleName
-    }
-
-    if ($PassThru) {
-        return $ModuleInfo.ToArray()
-    }
 }
 
 #endregion
