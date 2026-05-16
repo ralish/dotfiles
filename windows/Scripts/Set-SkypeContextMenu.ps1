586bd97cf906f72802dee292d181eebeeb12e7df
Samuel D. Leslie <sdl@nexiom.net>
2026-05-16 16:30:45 +1000

    Set-SkypeContextMenu: Error & string handling improvements

--- windows/Scripts/Set-SkypeContextMenu.ps1
+++ windows/Scripts/Set-SkypeContextMenu.ps1
@@ -7,36 +7,35 @@
 [CmdletBinding(SupportsShouldProcess)]
 [OutputType([Void])]
 Param(
+    [Parameter(Mandatory)]
     [ValidateSet('Enable', 'Disable')]
     [String]$Operation
 )
 
-if (![Environment]::OSVersion.Version -eq 10) {
-    throw 'Script is only valid for Windows 10 or later.'
+if ([Environment]::OSVersion.Version.Major -lt 10) {
+    $ErrMsg = 'Script is only valid for Windows 10 or later.'
+    $ErrCat = [Management.Automation.ErrorCategory]::NotInstalled
+    $ErrRec = [Management.Automation.ErrorRecord]::new([Exception]::new($ErrMsg), 'NotWin10OrLater', $ErrCat, $null)
+    $PSCmdlet.ThrowTerminatingError($ErrRec)
 }
 
 $ContextMenuPath = 'HKLM:\Software\Classes\PackagedCom\Package\Microsoft.SkypeApp_*\Class\{776DBC8D-7347-478C-8D71-791E12EF49D8}'
-
-try {
-    $ContextMenuKey = Get-ItemProperty -Path $ContextMenuPath -ErrorAction Stop
-} catch {
-    throw 'Failed to retrieve registry key for Skype context menu.'
-}
-
+$ContextMenuKey = Get-ItemProperty -Path $ContextMenuPath -ErrorAction 'Ignore'
 if (!$ContextMenuKey.DllPath) {
-    throw 'DllPath is empty or not-present for Skype context menu registry key.'
+    $ErrMsg = 'DllPath is empty or not-present for Skype context menu registry key.'
+    $ErrCat = [Management.Automation.ErrorCategory]::InvalidData
+    $ErrRec = [Management.Automation.ErrorRecord]::new([Exception]::new($ErrMsg), 'UnexpectedDllPathState', $ErrCat, $null)
+    $PSCmdlet.ThrowTerminatingError($ErrRec)
 }
 
 if ($Operation -eq 'Enable') {
     if ($PSCmdlet.ShouldProcess('Enable Skype context menu')) {
         if ($ContextMenuKey.DllPath.StartsWith('-')) {
-            Set-ItemProperty -Path $ContextMenuPath -Name 'DllPath' -Value $ContextMenuKey.DllPath.Substring(1)
+            Set-ItemProperty -Path $ContextMenuPath -Name 'DllPath' -Type 'String' -Value $ContextMenuKey.DllPath.Substring(1) -ErrorAction 'Stop'
         }
     }
-} else {
-    if ($PSCmdlet.ShouldProcess('Disable Skype context menu')) {
-        if (!$ContextMenuKey.DllPath.StartsWith('-')) {
-            Set-ItemProperty -Path $ContextMenuPath -Name 'DllPath' -Value ('-{0}' -f $ContextMenuKey.DllPath)
-        }
+} elseif ($PSCmdlet.ShouldProcess('Disable Skype context menu')) {
+    if (!$ContextMenuKey.DllPath.StartsWith('-')) {
+        Set-ItemProperty -Path $ContextMenuPath -Name 'DllPath' -Type 'String' -Value "-$($ContextMenuKey.DllPath)" -ErrorAction 'Stop'
     }
 }
