de86d52e954e8a82605b598de21d76b5380fcfb3
Samuel D. Leslie <sdl@nexiom.net>
2026-06-17 18:14:44 +1000

    Add support for Helm completions in PowerShell

---
+++ posh/Settings/80-Helm.ps1
@@ -0,0 +1,24 @@
+# Helm
+# https://helm.sh/
+# https://github.com/helm/helm
+
+$DotFilesSection = @{
+    Type    = 'Settings'
+    Name    = 'Helm'
+    Command = 'helm'
+}
+
+if (!(Start-DotFilesSection @DotFilesSection)) { Complete-DotFilesSection; return }
+
+# (Re)build the native completions script
+$CompletionsFile = Join-Path -Path $PoShCompletionsPath -ChildPath 'helm.ps1'
+if ($Env:DOTFILES_REBUILD_COMPLETIONS -or !(Test-Path -LiteralPath $CompletionsFile -PathType 'Leaf')) {
+    Write-Verbose -Message (Get-DotFilesMessage 'Building native completions script ...')
+    & helm completion powershell | Out-File -FilePath $CompletionsFile -Encoding 'utf8'
+}
+
+Write-Verbose -Message (Get-DotFilesMessage 'Registering native argument completer ...')
+Get-Content -LiteralPath $CompletionsFile | Out-String | Invoke-Expression # DevSkim: ignore DS104456
+
+Remove-Variable -Name 'CompletionsFile'
+Complete-DotFilesSection
