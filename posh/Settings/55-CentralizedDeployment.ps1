70f30cc1c9113da1982bfbe8ea0881ea39a1d71f
Samuel D. Leslie <sdl@nexiom.net>
2026-06-17 18:35:21 +1000

    Add alias for PowerShell connection to M365 Centralized Deployment

---
+++ posh/Settings/55-CentralizedDeployment.ps1
@@ -0,0 +1,15 @@
+# Centralized Deployment
+# https://learn.microsoft.com/en-au/microsoft-365/enterprise/use-the-centralized-deployment-powershell-cmdlets-to-manage-add-ins
+
+$DotFilesSection = @{
+    Type   = 'Settings'
+    Name   = 'Centralized Deployment'
+    Module = 'O365CentralizedAddInDeployment'
+}
+
+if (!(Start-DotFilesSection @DotFilesSection)) { Complete-DotFilesSection; return }
+
+# Add an alias for the unintuitively named `Connect-OrganizationAddInService`
+Set-Alias -Name 'Connect-CentralizedDeployment' -Value 'Connect-OrganizationAddInService'
+
+Complete-DotFilesSection
