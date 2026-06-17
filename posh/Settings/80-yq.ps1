bc4beb23c2ba8b9e3e261b9c55b18ddb7763d9d8
Samuel D. Leslie <sdl@nexiom.net>
2026-06-17 18:16:56 +1000

    Add support for yq completions in PowerShell

---
+++ posh/Settings/80-yq.ps1
@@ -0,0 +1,23 @@
+# yq
+# https://github.com/mikefarah/yq
+
+$DotFilesSection = @{
+    Type    = 'Settings'
+    Name    = 'yq'
+    Command = 'yq'
+}
+
+if (!(Start-DotFilesSection @DotFilesSection)) { Complete-DotFilesSection; return }
+
+# (Re)build the native completions script
+$CompletionsFile = Join-Path -Path $PoShCompletionsPath -ChildPath 'yq.ps1'
+if ($Env:DOTFILES_REBUILD_COMPLETIONS -or !(Test-Path -LiteralPath $CompletionsFile -PathType 'Leaf')) {
+    Write-Verbose -Message (Get-DotFilesMessage 'Building native completions script ...')
+    & yq shell-completion powershell | Out-File -FilePath $CompletionsFile -Encoding 'utf8'
+}
+
+Write-Verbose -Message (Get-DotFilesMessage 'Registering native argument completer ...')
+Get-Content -LiteralPath $CompletionsFile | Out-String | Invoke-Expression # DevSkim: ignore DS104456
+
+Remove-Variable -Name 'CompletionsFile'
+Complete-DotFilesSection
