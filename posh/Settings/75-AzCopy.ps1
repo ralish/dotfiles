5bb5be9e1b0cdb01f26dea4be953f556f6d8b168
Samuel D. Leslie <sdl@nexiom.net>
2026-06-17 17:36:15 +1000

    Add PowerShell native completions support for AzCopy

--- posh/Settings/75-AzCopy.ps1
+++ posh/Settings/75-AzCopy.ps1
@@ -13,4 +13,15 @@ if (!(Start-DotFilesSection @DotFilesSection)) { Complete-DotFilesSection; retur
 # Disable logging to the system log
 $Env:AZCOPY_DISABLE_SYSLOG = 'true'
 
+# (Re)build the native completions script
+$CompletionsFile = Join-Path -Path $PoShCompletionsPath -ChildPath 'azcopy.ps1'
+if ($Env:DOTFILES_REBUILD_COMPLETIONS -or !(Test-Path -LiteralPath $CompletionsFile -PathType 'Leaf')) {
+    Write-Verbose -Message (Get-DotFilesMessage 'Building native completions script ...')
+    & azcopy completion powershell | Out-File -FilePath $CompletionsFile -Encoding 'utf8'
+}
+
+Write-Verbose -Message (Get-DotFilesMessage 'Registering native argument completer ...')
+Get-Content -LiteralPath $CompletionsFile | Out-String | Invoke-Expression # DevSkim: ignore DS104456
+
+Remove-Variable -Name 'CompletionsFile'
 Complete-DotFilesSection
